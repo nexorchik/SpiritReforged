e11044b958fdb2e4b73fbb640605f0b151d6bf04
GabeHasWon <gabriel@guevarameyer.com>
2024-08-09 13:11:53 -0500

    ectotone map

---
+++ Common/WorldGeneration/Ecotones/EcotoneEdgeDefinition.cs
@@ -0,0 +1,50 @@
+﻿namespace SpiritReforged.Common.WorldGeneration.Ecotones;
+
+public readonly struct EcotoneEdgeDefinition(int displayId, string name, params int[] validIds)
+{
+	public readonly string Name = name;
+	public readonly int[] ValidIds = validIds;
+	public readonly int DisplayId = displayId;
+
+	public override string ToString() => Name + $"({DisplayId})";
+}
+
+public class EcotoneEdgeDefinitions : ILoadable
+{
+	private static Dictionary<string, EcotoneEdgeDefinition> _ecotonesByName = [];
+	private static Dictionary<int, EcotoneEdgeDefinition> _ecotonesByValidTileIds = [];
+	private static HashSet<int> _registeredIds = [];
+
+	public static void AddEcotone(EcotoneEdgeDefinition def)
+	{
+		_ecotonesByName.Add(def.Name, def);
+
+		foreach (int item in def.ValidIds)
+		{
+			_ecotonesByValidTileIds.Add(item, def);
+			_registeredIds.Add(item);
+		}
+	}
+
+	public static EcotoneEdgeDefinition GetEcotone(string name) => _ecotonesByName[name];
+	public static EcotoneEdgeDefinition GetEcotoneByTile(int id) => _ecotonesByValidTileIds[id];
+	public static bool TryGetEcotoneByTile(int id, out EcotoneEdgeDefinition def) => _ecotonesByValidTileIds.TryGetValue(id, out def);
+	public static bool TileRegistered(int id) => _registeredIds.Contains(id);
+
+	public void Load(Mod mod)
+	{
+		AddEcotone(new EcotoneEdgeDefinition(TileID.Dirt, "Forest", TileID.Grass, TileID.Dirt, TileID.ClayBlock));
+		AddEcotone(new EcotoneEdgeDefinition(TileID.Sandstone, "Desert", TileID.Sand));
+		AddEcotone(new EcotoneEdgeDefinition(TileID.SnowBlock, "Snow", TileID.SnowBlock, TileID.IceBlock));
+		AddEcotone(new EcotoneEdgeDefinition(TileID.ChlorophyteBrick, "Jungle", TileID.JungleGrass));
+		AddEcotone(new EcotoneEdgeDefinition(TileID.DemoniteBrick, "Corruption", TileID.CorruptGrass, TileID.Ebonsand, TileID.Ebonstone, TileID.CorruptIce));
+		AddEcotone(new EcotoneEdgeDefinition(TileID.CrimtaneBrick, "Crimson", TileID.CrimsonGrass, TileID.Crimsand, TileID.Crimstone, TileID.FleshIce));
+	}
+
+	public void Unload()
+	{
+		_ecotonesByName = null;
+		_ecotonesByValidTileIds = null;
+		_registeredIds = null;
+	}
+}
---
+++ Common/WorldGeneration/Ecotones/EcotoneSurfaceMapping.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using Terraria.GameContent.Generation;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+
+namespace SpiritReforged.Common.WorldGeneration.Ecotones;
+
+internal class EcotoneSurfaceMapping : ModSystem
+{
+	public class EcotoneEntry(Point start, EcotoneEdgeDefinition definition)
+	{
+		public Point Start = start;
+		public Point End;
+		public HashSet<Point> SurfacePoints = [];
+		public EcotoneEdgeDefinition Definition = definition;
+
+		public bool TileFits(int i, int j) => Definition.ValidIds.Contains(Main.tile[i, j].TileType);
+
+		public override string ToString() => $"{Start} to {End}; of {Definition}";
+	}
+
+	public List<EcotoneEntry> Entries = [];
+
+	public override void ModifyWorldGenTasks(List<GenPass> tasks, ref double totalWeight)
+	{
+		int index = tasks.FindIndex(x => x.Name == "Tile Cleanup");
+
+		if (index != -1)
+			tasks.Insert(index + 1, new PassLegacy("Map Ecotones", MapEcotones));
+	}
+
+	private void MapEcotones(GenerationProgress progress, GameConfiguration configuration)
+	{
+		progress.Message = "Mapping ecotones";
+
+		int y = 0;
+
+		while (Main.tile[150, ++y].TileType != TileID.Sand)
+		{
+		}
+
+		int transitionCount = 0;
+		EcotoneEntry entry = new EcotoneEntry(new Point(150, y), EcotoneEdgeDefinitions.GetEcotone("Desert"));
+
+		for (int x = 150; x < Main.maxTilesX - 150; ++x)
+		{
+			int dir = WorldGen.SolidOrSlopedTile(x, y) ? -1 : 1;
+
+			if (dir == -1)
+			{
+				while (SolidTileOrWall(x, y))
+					y -= 1;
+
+				y -= dir;
+			}
+			else
+			{
+				while (!SolidTileOrWall(x, y))
+					y += 1;
+			}
+
+			if (!entry.TileFits(x, y))
+				transitionCount++;
+
+			if (transitionCount > 20 && EcotoneEdgeDefinitions.TryGetEcotoneByTile(Main.tile[x, y].TileType, out var newDefinition))
+			{
+				entry.End = new Point(x, y);
+				Entries.Add(entry);
+				entry = new EcotoneEntry(new Point(x, y), newDefinition);
+				transitionCount = 0;
+			}
+
+			WorldGen.PlaceTile(x, y - 2, entry.Definition.DisplayId, true, true);
+			WorldGen.PlaceTile(x, y - 3, entry.Definition.DisplayId, true, true);
+			entry.SurfacePoints.Add(new Point(x, y));
+		}
+
+		foreach (var item in Entries)
+		{
+			for (int x = item.Start.X; x < item.End.X; ++x)
+			{
+				for (int nY = 40; nY < 80; ++nY)
+					WorldGen.PlaceTile(x, nY, item.Definition.DisplayId, true, true);
+			}
+		}
+	}
+
+	private static bool SolidTileOrWall(int x, int y) => WorldGen.SolidOrSlopedTile(x, y) || Main.tile[x, y].WallType != WallID.None;
+}
