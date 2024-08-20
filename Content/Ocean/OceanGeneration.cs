e8628be5de840bcc8560e14362b4dab95949405c
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-08-20 17:14:56 -0300

    Ocean cave gen

--- Content/Ocean/OceanGeneration.cs
+++ Content/Ocean/OceanGeneration.cs
@@ -30,8 +30,15 @@ public class OceanGeneration : ModSystem
                 tasks[beachIndex] = new PassLegacy("Beaches", GenerateOcean);
 
             tasks.RemoveAt(tasks.FindIndex(genpass => genpass.Name.Equals("Shell Piles")));
-        }
-    }
+
+			#region ocean caves
+			int cavesIndex = tasks.FindIndex(genpass => genpass.Name.Equals("Create Ocean Caves")); //Replace ocean cave gen
+
+			if (cavesIndex != -1)
+				tasks[cavesIndex] = new PassLegacy("Create Ocean Caves", GenerateOceanCaves);
+			#endregion
+		}
+	}
 
 	/// <summary>Generates the Ocean ("Beaches"). Heavily based on vanilla code.</summary>
 	/// <param name="progress"></param>
@@ -211,7 +218,7 @@ public class OceanGeneration : ModSystem
 				}
 
 				//Hydrothermal vents
-				if (WorldGen.genRand.Next(7) < 3 && tilesFromInnerEdge > 135 && ValidGround(i, j, 1, TileID.Sand) && OpenArea(i, j - 3, 1, 3))
+				/*if (WorldGen.genRand.Next(7) < 3 && tilesFromInnerEdge > 135 && ValidGround(i, j, 1, TileID.Sand) && OpenArea(i, j - 3, 1, 3))
 				{
 					int type = WorldGen.genRand.NextBool(3) ? ModContent.TileType<HydrothermalVent1x3>() : ModContent.TileType<HydrothermalVent1x2>();
 					int offset = type == ModContent.TileType<HydrothermalVent1x2>() ? 1 : 2;
@@ -219,7 +226,7 @@ public class OceanGeneration : ModSystem
 					WorldGen.PlaceObject(i, j - offset, type, true, WorldGen.genRand.Next(2));
 					NetMessage.SendObjectPlacement(-1, i, j, type, 0, 0, -1, -1);
 					continue;
-				}
+				}*/
 
 				//Growing kelp
 				if (WorldGen.genRand.Next(5) < 2 && tilesFromInnerEdge < 133 && ValidGround(i, j, 1, TileID.Sand))
@@ -458,6 +465,62 @@ public class OceanGeneration : ModSystem
 				returnValue = LastSlope(160);
 
 			return -returnValue;
+		}
+	}
+
+	/// <summary>Generates ocean caves like vanilla does but with a guaranteed chance.</summary>
+	public static void GenerateOceanCaves(GenerationProgress progress, GameConfiguration config)
+	{
+		static void Decorate(int i, int j)
+		{
+			for (int x = 0; x < 300; x++)
+			{
+				var on = Framing.GetTileSafely(i, j);
+				var below = Framing.GetTileSafely(i, j + 1);
+
+				for (int a = 0; a < 50; a++) //Position vertically
+				{
+					on = Framing.GetTileSafely(i, j);
+					below = Framing.GetTileSafely(i, j + 1);
+
+					if (on.HasTile && Main.tileSolid[on.TileType])
+						j--;
+					else if (!below.HasTile || !Main.tileSolid[below.TileType])
+						j++;
+					else
+						break;
+				}
+
+				//Hydrothermal vents
+				if (on.LiquidAmount > 0 && on.LiquidType == LiquidID.Water && below.TileType == TileID.Sand && !below.TopSlope && WorldGen.genRand.NextBool(25))
+				{
+					int type = WorldGen.genRand.NextBool(3) ? ModContent.TileType<HydrothermalVent1x3>() : ModContent.TileType<HydrothermalVent1x2>();
+
+					WorldGen.PlaceObject(i, j, type, true, WorldGen.genRand.Next(2));
+					NetMessage.SendObjectPlacement(-1, i, j, type, 0, 0, -1, -1);
+				}
+
+				i -= GenVars.dungeonSide; //Position horizontally
+			}
+		}
+
+		for (int attempt = 0; attempt < 2; attempt++)
+		{
+			if ((attempt != 0 || GenVars.dungeonSide <= 0) && (attempt != 1 || GenVars.dungeonSide >= 0))
+			{
+				progress.Message = Lang.gen[90].Value;
+				int i = WorldGen.genRand.Next(55, 95);
+				if (attempt == 1)
+					i = WorldGen.genRand.Next(Main.maxTilesX - 95, Main.maxTilesX - 55);
+
+				int j;
+				for (j = 0; !Main.tile[i, j].HasTile; j++)
+				{
+				}
+
+				WorldGen.oceanCave(i, j);
+				Decorate(i, j);
+			}
 		}
 	}
 
