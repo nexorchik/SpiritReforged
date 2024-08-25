ba0683bea0e338acb239d1254724856c2510e697
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-08-25 15:23:34 -0300

    Hanging Cloudstalk wind sway

--- Content/Cloudstalk/Items/HangingCloudstalk.cs
+++ Content/Cloudstalk/Items/HangingCloudstalk.cs
@@ -1,4 +1,5 @@
 using Terraria.DataStructures;
+using Terraria.GameContent.Drawing;
 
 namespace SpiritReforged.Content.Cloudstalk.Items;
 
@@ -9,7 +10,7 @@ public class HangingCloudstalk : ModItem
 		Item.DefaultToPlaceableTile(ModContent.TileType<HangingCloudstalkTile>());
 		Item.width = 36;
 		Item.height = 28;
-		Item.value = Item.value = Item.sellPrice(0, 0, 1, 50);
+		Item.value = Item.sellPrice(0, 0, 1, 50);
 	}
 
 	public override void AddRecipes()
@@ -23,6 +24,23 @@ public class HangingCloudstalk : ModItem
 
 public class HangingCloudstalkTile : ModTile
 {
+	private double windSway;
+
+	public override void Load() => On_TileDrawing.Update += UpdateTileDrawing;
+
+	private void UpdateTileDrawing(On_TileDrawing.orig_Update orig, TileDrawing self)
+	{
+		if (!Main.dedServ) //Wind speed calculation
+		{
+			double num = Math.Abs(Main.WindForVisuals);
+
+			num = Utils.GetLerpValue(0.08f, 1.2f, (float)num, clamped: true);
+			windSway += 1.0 / 420.0 + 1.0 / 420.0 * num * 5.0; //Sunflower speed
+		}
+
+		orig(self);
+	}
+
 	public override void SetStaticDefaults()
 	{
 		Main.tileTable[Type] = true;
@@ -52,4 +70,85 @@ public class HangingCloudstalkTile : ModTile
 	}
 
 	public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b) => (r, g, b) = (0.2f, 0.2f, 0.4f);
+
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		Tile tile = Framing.GetTileSafely(i, j);
+		var topLeft = new Point16(i - tile.TileFrameX % 36 / 18, j - tile.TileFrameY / 18);
+
+		if (new Point16(i, j) != topLeft) //Draw only once on the multitile origin so we have control over iterations
+			return false;
+
+		spriteBatch.End();
+		spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Matrix.Identity);
+		//Restart the spritebatch so the tile isn't blurry
+
+		float GetWindSway()
+		{
+			float rotation = Main.instance.TilesRenderer.GetWindCycle(topLeft.X, topLeft.Y, windSway);
+
+			if (!WorldGen.InAPlaceWithWind(topLeft.X, topLeft.Y, 2, 3))
+				rotation = 0f;
+
+			return rotation + GetHighestWindGridPushComplex(topLeft.X, topLeft.Y, 2, 3, 60, 1.26f, 3, true);
+			//Main.instance.TilesRenderer.GetWindGridPushComplex(topLeft.X, topLeft.Y, 60, 1.26f, 3, true);
+		}
+
+		float windCycle = GetWindSway();
+		Texture2D texture = TextureAssets.Tile[Type].Value;
+		Vector2 offset = (Lighting.LegacyEngine.Mode > 1 && Main.GameZoomTarget == 1) ? Vector2.Zero : Vector2.One * 12;
+
+		int _i = i;
+		int _j = j;
+
+		for (int y = 0; y < 3; y++)
+		{
+			for (int x = 0; x < 2; x++)
+			{
+				(i, j) = (_i + x, _j + y);
+
+				tile = Framing.GetTileSafely(i, j);
+
+				var source = new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, 16);
+				var drawPos = new Vector2((i + offset.X) * 16 - (int)Main.screenPosition.X, (j + offset.Y) * 16 - (int)Main.screenPosition.Y);
+
+				var origin = new Vector2(16 - tile.TileFrameX % 36 / 18 * 16, -(tile.TileFrameY / 18 * 16));
+				drawPos += origin;
+				drawPos += new Vector2(TileObjectData.GetTileData(tile.TileType, 0).DrawXOffset, TileObjectData.GetTileData(tile.TileType, 0).DrawYOffset);
+
+				float swing = 3f * (j - topLeft.Y + 1) / 54f;
+				float rotation = -windCycle * MathHelper.Max(swing, .1f);
+
+				drawPos += new Vector2(windCycle, Math.Abs(windCycle) * -4f * swing);
+
+				spriteBatch.Draw(texture, drawPos, source, Lighting.GetColor(i, j), rotation, origin, 1, SpriteEffects.None, 0f);
+			}
+		}
+
+		spriteBatch.End();
+		spriteBatch.Begin();
+
+		return false;
+	}
+
+	private static float GetHighestWindGridPushComplex(int topLeftX, int topLeftY, int sizeX, int sizeY, int totalPushTime, float pushForcePerFrame, int loops, bool swapLoopDir) //Adapted from vanilla
+	{
+		float result = 0f;
+		int num = int.MaxValue;
+		for (int i = 0; i < 1; i++)
+		{
+			for (int j = 0; j < sizeY; j++)
+			{
+				Main.instance.TilesRenderer.Wind.GetWindTime(topLeftX + i + sizeX / 2, topLeftY + j, totalPushTime, out var windTimeLeft, out var _, out var _);
+				float windGridPushComplex = Main.instance.TilesRenderer.GetWindGridPushComplex(topLeftX + i, topLeftY + j, totalPushTime, pushForcePerFrame, loops, swapLoopDir);
+				if (windTimeLeft < num && windTimeLeft != 0)
+				{
+					result = windGridPushComplex;
+					num = windTimeLeft;
+				}
+			}
+		}
+
+		return result;
+	}
 }
