09d3048604c5e3847649219529938c59516cd267
zoroarkcity <zoroarkcity2002@gmail.com>
2024-08-26 21:39:23 -0700

    Initial port but i get the branch right for sure this time

---
+++ Common/Particle/Particle.cs
@@ -0,0 +1,56 @@
+﻿namespace SpiritReforged.Common.Particle;
+
+/// <summary>
+/// Represents a particle with a position, velocity, rotation, scale and transparency.
+/// TL;DR: Better dust.
+/// </summary>
+public class Particle
+{
+	public int ID; // you don't have to use this
+	public int Type; // you don't have to use this
+	public Vector2 Position;
+	public Vector2 Velocity;
+	public Vector2 Origin;
+	public Color Color;
+	public float Rotation;
+	public float Scale;
+	public uint TimeActive;
+	public ParticleLayer DrawLayer;
+
+	public Texture2D Texture => ParticleHandler.GetTexture(Type);
+
+	/// <summary>
+	/// Determines whether the particle is drawn using its default drawing method, the default drawing method with additive color blending, or a custom drawing type.
+	/// If set to Custom, the CustomDraw method will be called.
+	/// </summary>
+	public virtual ParticleDrawType DrawType => ParticleDrawType.DefaultAlphaBlend;
+
+	/// <summary>
+	/// The chance at any given tick that this particle will spawn.
+	/// Return 0f if you want the particle to not naturally spawn (if you want to spawn it yourself).
+	/// This hook will not run if Particle.ActiveCondition returns false.
+	/// </summary>
+	public virtual float SpawnChance => 0f;
+
+	/// <summary>
+	/// Call this when you want to clear your particle and remove it from the world.
+	/// </summary>
+	public void Kill() => ParticleHandler.DeleteParticleAtIndex(ID);
+
+	/// <summary>
+	/// Called every tick. Update your particle in this method.
+	/// Particle velocity is automatically added to the particle position for you, and TimeAlive is incremented.
+	/// </summary>
+	public virtual void Update() { }
+
+	/// <summary>
+	/// Allows you to do custom drawing for your particle. Only called if Particle.UseCustomDrawing is true.
+	/// </summary>
+	public virtual void CustomDraw(SpriteBatch spriteBatch) { }
+
+	/// <summary>
+	/// Called if the particle tries to naturally spawn. This can only be called if Particle.SpawnChance returns a value greater than 0f and Particle.ActiveCondition is true.
+	/// Use this to spawn your particle randomly.
+	/// </summary>
+	public virtual void OnSpawnAttempt() { }
+}
---
+++ Common/Particle/ParticleHandler.cs
@@ -0,0 +1,183 @@
+﻿namespace SpiritReforged.Common.Particle;
+
+public enum ParticleLayer
+{
+	AboveDust,
+	AboveProjectile,
+	AboveNPC,
+	AbovePlayer
+}
+
+public enum ParticleDrawType
+{
+	DefaultAlphaBlend,
+	DefaultAdditive,
+	Custom
+}
+
+public static class ParticleHandler
+{
+	private static readonly int MaxParticlesAllowed = 500;
+
+	private static Particle[] particles;
+	private static int nextVacantIndex;
+	private static int activeParticles;
+	private static Dictionary<Type, int> particleTypes;
+	private static Dictionary<int, Texture2D> particleTextures;
+
+	internal static void RegisterParticles()
+	{
+		particles = new Particle[MaxParticlesAllowed];
+		particleTypes = [];
+		particleTextures = [];
+
+		Type baseParticleType = typeof(Particle);
+		SpiritReforgedMod spiritMod = ModContent.GetInstance<SpiritReforgedMod>();
+
+		foreach (Type type in spiritMod.Code.GetTypes())
+			if (type.IsSubclassOf(baseParticleType) && !type.IsAbstract && type != baseParticleType)
+			{
+				int assignedType = particleTypes.Count;
+				particleTypes[type] = assignedType;
+
+				string texturePath = type.Namespace.Replace('.', '/') + "/" + type.Name;
+				var particleTexture = ModContent.RequestIfExists(texturePath, out Asset<Texture2D> tex, AssetRequestMode.ImmediateLoad) ? tex.Value 
+					: ModContent.Request<Texture2D>("SpiritReforged/Assets/Textures/ParticleDefault", AssetRequestMode.ImmediateLoad).Value;
+
+				particleTextures[assignedType] = particleTexture;
+			}
+	}
+
+	internal static void Unload()
+	{
+		particles = null;
+		particleTypes = null;
+		particleTextures = null;
+	}
+
+	/// <summary>
+	/// Spawns the particle instance provided into the world (if the particle limit is not reached).
+	/// </summary>
+	public static void SpawnParticle(Particle particle)
+	{
+		if (Main.netMode == NetmodeID.Server || activeParticles == MaxParticlesAllowed)
+			return;
+
+		particles[nextVacantIndex] = particle;
+		particle.ID = nextVacantIndex;
+		particle.Type = particleTypes[particle.GetType()];
+
+		if (nextVacantIndex + 1 < particles.Length && particles[nextVacantIndex + 1] == null)
+			nextVacantIndex++;
+		else
+			for (int i = 0; i < particles.Length; i++)
+				if (particles[i] == null)
+					nextVacantIndex = i;
+
+		activeParticles++;
+	}
+
+	public static void SpawnParticle(int type, Vector2 position, Vector2 velocity, Vector2 origin = default, float rotation = 0f, float scale = 1f)
+	{
+		var particle = new Particle(); // yes i know constructors exist. yes i'm doing this so you dont have to make constructors over and over.
+		particle.Position = position;
+		particle.Velocity = velocity;
+		particle.Color = Color.White;
+		particle.Origin = origin;
+		particle.Rotation = rotation;
+		particle.Scale = scale;
+		particle.Type = type;
+
+		SpawnParticle(particle);
+	}
+
+	public static void SpawnParticle(int type, Vector2 position, Vector2 velocity)
+	{
+		var particle = new Particle();
+		particle.Position = position;
+		particle.Velocity = velocity;
+		particle.Color = Color.White;
+		particle.Origin = Vector2.Zero;
+		particle.Rotation = 0f;
+		particle.Scale = 1f;
+		particle.Type = type;
+
+		SpawnParticle(particle);
+	}
+
+	/// <summary>
+	/// Deletes the particle at the given index. You typically do not have to use this; use Particle.Kill() instead.
+	/// </summary>
+	public static void DeleteParticleAtIndex(int index)
+	{
+		particles[index] = null;
+		activeParticles--;
+		nextVacantIndex = index;
+	}
+
+	/// <summary>
+	/// Clears all the currently spawned particles.
+	/// </summary>
+	public static void ClearAllParticles()
+	{
+		for (int i = 0; i < particles.Length; i++)
+			particles[i] = null;
+
+		activeParticles = 0;
+		nextVacantIndex = 0;
+	}
+
+	internal static void UpdateAllParticles()
+	{
+		foreach (Particle particle in particles)
+		{
+			if (particle == null)
+				continue;
+
+			particle.TimeActive++;
+			particle.Position += particle.Velocity;
+
+			particle.Update();
+		}
+	}
+
+	internal static void DrawAllParticles(SpriteBatch spriteBatch, ParticleLayer drawLayer)
+	{
+		foreach (Particle particle in particles)
+		{
+			if (particle == null)
+				continue;
+
+			if(particle.DrawLayer == drawLayer)
+			{
+				Color additiveColor = particle.Color;
+				additiveColor.A = 0;
+
+				switch (particle.DrawType)
+				{
+					case ParticleDrawType.DefaultAlphaBlend:
+						spriteBatch.Draw(particleTextures[particle.Type], particle.Position - Main.screenPosition, null, particle.Color, particle.Rotation, particle.Origin, particle.Scale * Main.GameViewMatrix.Zoom, SpriteEffects.None, 0f);
+						break;
+
+					case ParticleDrawType.DefaultAdditive:
+						spriteBatch.Draw(particleTextures[particle.Type], particle.Position - Main.screenPosition, null, particle.Color, particle.Rotation, particle.Origin, particle.Scale * Main.GameViewMatrix.Zoom, SpriteEffects.None, 0f);
+						break;
+
+					case ParticleDrawType.Custom:
+						particle.CustomDraw(spriteBatch);
+						break;
+				}
+			}
+		}
+	}
+
+	/// <summary>
+	/// Gets the texture of the given particle type.
+	/// </summary>
+	public static Texture2D GetTexture(int type) => particleTextures[type];
+
+	/// <summary>
+	/// Returns the numeric type of the given particle.
+	/// </summary>
+	public static int ParticleType<T>() => particleTypes[typeof(T)];
+}
