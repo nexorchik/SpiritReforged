860645b45dd29def682422ae38675ca51fcdd26e
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-08-28 23:05:55 -0300

    Fix

--- Content/Savanna/DustStorm/DustStormPlayer.cs
+++ Content/Savanna/DustStorm/DustStormPlayer.cs
@@ -5,6 +5,8 @@ namespace SpiritReforged.Content.Savanna.DustStorm;
 
 public class DustStormPlayer : ModPlayer
 {
+	private bool wasDustStorm;
+
 	/// <summary> Whether the player is present in a dust storm. </summary>
 	public bool ZoneDustStorm => Math.Abs(Main.windSpeedCurrent) > .4f && Player.InModBiome<Biome.SavannaBiome>();
 
@@ -18,25 +20,28 @@ public class DustStormPlayer : ModPlayer
 
 			if (!Filters.Scene[sandstorm].IsActive()) //Initialize
 			{
+				//Severity has a value even if it isn't being used. Doing this prevents snapping to high-severity sandstorm vfx upon entering a dust storm zone
+				if (!Filters.Scene[sandstorm].IsInUse())
+					Sandstorm.Severity = intensity;
+
 				var center = Player.Center;
 
 				SkyManager.Instance.Activate(sandstorm, center);
 				Filters.Scene.Activate(sandstorm, center);
 				Overlays.Scene.Activate(sandstorm, center); //Might have no effect?
-
-				//Severity has a value even if it isn't being used. Doing this prevents snapping to high-severity sandstorm vfx upon entering a dust storm zone
-				if (!Filters.Scene[sandstorm].IsInUse())
-					Sandstorm.Severity = intensity;
 			}
 
 			if (!Player.ZoneSandstorm)
 				Sandstorm.Severity = MathHelper.Max(Sandstorm.Severity - .01f, intensity); //Transition into a calmer severity if necessary
 		}
-		else if (Filters.Scene[sandstorm].IsActive() && !Filters.Scene[sandstorm].IsInUse())
+
+		if (!ZoneDustStorm && !wasDustStorm && Filters.Scene[sandstorm].IsActive())
 		{
 			SkyManager.Instance.Deactivate(sandstorm);
 			Filters.Scene.Deactivate(sandstorm);
 			Overlays.Scene.Deactivate(sandstorm);
 		}
+
+		wasDustStorm = ZoneDustStorm;
 	}
 }
