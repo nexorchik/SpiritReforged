602b81c614a445c63df3e21ca86c71fb54adcb9b
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-09-09 20:21:27 -0300

    Ambivalent Tumbler

---
+++ Content/Savanna/Items/WrithingSticks/TumblerPetBuff.cs
@@ -0,0 +1,8 @@
+using SpiritReforged.Common.ProjectileCommon;
+
+namespace SpiritReforged.Content.Savanna.Items.WrithingSticks;
+
+public class TumblerPetBuff : BasePetBuff<TumblerPet>
+{
+	protected override (string, string) BuffInfo => ("", "");
+}
