4fdada4dd66e26dbeb884ecdd94d76960ff1bafa
GabeHasWon <gabriel@guevarameyer.com>
2024-09-10 18:42:18 -0500

    glowmask NPC

---
+++ Common/Visuals/Glowmasks/AutoloadGlowmaskAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace SpiritReforged.Common.Visuals.Glowmasks;
+
+/// <summary>
+/// Autoloads the glowmask of the according Entity (currently, only NPCs).<br/>
+/// <paramref name="stringData"/> is one of two things. First, the Color of the glowmask, if constant, in one of these formats:<br/>
+/// <c>R,G,B,A</c><br/><c>R,G,B</c><br/>All numbers are bytes.<br/><br/>
+/// If you want a dynamic color, you can create a static method that takes an <c>NPC</c> and returns <c>Color</c>.<br/>
+/// You'll need a fully qualified class name and the name of the method, prepended by "Method:". For example,
+/// <code>[AutoloadGlowmask("Method:Content.Savanna.NPCs.Gar.Gar Glow")]</code><br/>
+/// This would call <c>Gar.Glow</c>. 
+/// </summary>
+/// <param name="stringData"></param>
+[AttributeUsage(AttributeTargets.Class)]
+internal class AutoloadGlowmaskAttribute(string stringData) : Attribute
+{
+	public string StringData = stringData;
+}
---
+++ Common/Visuals/Glowmasks/GlowmaskNPC.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Reflection;
+using Terraria.ModLoader.Core;
+
+namespace SpiritReforged.Common.Visuals.Glowmasks;
+
+internal class GlowmaskNPC : GlobalNPC
+{
+	public readonly struct GlowmaskInfo(Asset<Texture2D> glowmask, Func<NPC, Color> drawColor)
+	{
+		public readonly Asset<Texture2D> Glowmask = glowmask;
+		public readonly Func<NPC, Color> GetDrawColor = drawColor;
+	}
+
+	public static Dictionary<int, GlowmaskInfo> NpcIdToGlowmask = [];
+
+	public override void SetStaticDefaults()
+	{
+		var types = AssemblyManager.GetLoadableTypes(Mod.Code).Where(x => typeof(ModNPC).IsAssignableFrom(x) && !x.IsAbstract
+			&& Attribute.IsDefined(x, typeof(AutoloadGlowmaskAttribute)));
+		var getId = typeof(ModContent).GetMethod(nameof(ModContent.NPCType));
+		var searchFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+
+		foreach (var type in types)
+		{
+			int id = (int)getId.MakeGenericMethod(type).Invoke(null, null);
+			string colString = (Attribute.GetCustomAttribute(type, typeof(AutoloadGlowmaskAttribute)) as AutoloadGlowmaskAttribute).StringData;
+			Func<NPC, Color> color;
+			
+			if (!colString.StartsWith("Method:"))
+				color = (_) => GetColorFromString(colString);
+			else
+			{
+				string[] split = colString.Split(' ');
+				var colorMethod = Mod.Code.GetType("SpiritReforged." + split[0]["Method:".Length..]).GetMethod(split[1], searchFlags);
+				color = Delegate.CreateDelegate(typeof(Func<NPC, Color>), null, colorMethod) as Func<NPC, Color>;
+			}
+
+			NpcIdToGlowmask.Add(id, new(ModContent.Request<Texture2D>(ModContent.GetModNPC(id).Texture + "_Glow"), color));
+		}
+	}
+
+	private static Color GetColorFromString(string colorString)
+	{
+		string[] rgba = colorString.Split(',');
+
+		if (rgba.Length <= 2 || rgba.Length >= 5)
+			throw new InvalidCastException("GlowmaskAttribute GlowColorString should be R,G,B or R,G,B,A!");
+
+		byte r = byte.Parse(rgba[0]);
+		byte g = byte.Parse(rgba[1]);
+		byte b = byte.Parse(rgba[2]);
+
+		if (rgba.Length == 3)
+			return new Color(r, g, b);
+
+		return new Color(r, g, b, byte.Parse(rgba[3]));
+	}
+
+	public override void PostDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+	{
+		if (NpcIdToGlowmask.TryGetValue(npc.type, out var glow)) 
+		{
+			Vector2 pos = npc.Center - screenPos + new Vector2(0, npc.gfxOffY);
+			SpriteEffects effect = npc.spriteDirection == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+			spriteBatch.Draw(glow.Glowmask.Value, pos, npc.frame, glow.GetDrawColor(npc), npc.rotation, npc.frame.Size() / 2f, npc.scale, effect, 0);
+		}
+	}
+}
