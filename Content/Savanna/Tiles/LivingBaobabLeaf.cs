4c7e792fe917d6b92577359f22f4c96d822457c3
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-09-10 17:39:03 -0300

    Baobab fruit + Devourer of Soil

--- Content/Savanna/Tiles/LivingBaobabLeaf.cs
+++ Content/Savanna/Tiles/LivingBaobabLeaf.cs
@@ -12,4 +12,11 @@ internal class LivingBaobabLeaf : ModTile
 		AddMapEntry(new Color(140, 156, 55));
 		HitSound = SoundID.Dig;
 	}
+
+	public override void RandomUpdate(int i, int j)
+	{
+		//Randomly grow hanging baobab fruit
+		if (Main.rand.NextBool(50) && !Framing.GetTileSafely(i, j + 1).HasTile && !Framing.GetTileSafely(i, j + 2).HasTile)
+			WorldGen.PlaceObject(i, j + 1, ModContent.TileType<Items.BaobabFruit.BaobabFruitTile>(), true);
+	}
 }
