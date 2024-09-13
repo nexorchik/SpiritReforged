28464a92d2f0a8a078e4284bcca5a8b1d108a63f
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-09-12 22:03:50 -0300

    Update Beach Umbrella

---
+++ Common/TileCommon/DrawPreviewHook/DrawPreviewHook.cs
@@ -0,0 +1,24 @@
+﻿namespace SpiritReforged.Common.TileCommon.DrawPreviewHook;
+
+public class DrawPreviewHook : ILoadable
+{
+	public static void ForceDrawPreview()
+	{
+		var offscreen = new Vector2(Main.offScreenRange);
+		var obj = TileObject.objectPreview;
+
+		TileObject.DrawPreview(Main.spriteBatch, obj, Main.Camera.UnscaledPosition - offscreen);
+	}
+
+	public void Load(Mod mod) => On_TileObject.DrawPreview += On_TileObject_DrawPreview;
+
+	private void On_TileObject_DrawPreview(On_TileObject.orig_DrawPreview orig, SpriteBatch sb, Terraria.DataStructures.TileObjectPreviewData op, Vector2 position)
+	{
+		if (TileLoader.GetTile(op.Type) is IDrawPreview idp)
+			idp.DrawPreview(sb, op, position);
+		else
+			orig(sb, op, position);
+	}
+
+	public void Unload() { }
+}
