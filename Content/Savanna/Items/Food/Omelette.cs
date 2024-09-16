b46899b8be87490a570d2c78ba164f0cb196f9c6
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-09-16 12:05:51 -0300

    Fix redundant FruitItemsSet usage + minor fixes

--- Content/Savanna/Items/Food/Omelette.cs
+++ Content/Savanna/Items/Food/Omelette.cs
@@ -5,10 +5,11 @@ namespace SpiritReforged.Content.Savanna.Items.Food;
 public class Omelette : FoodItem
 {
 	internal override Point Size => new(36, 24);
+
 	public override void Defaults()
 	{
-		FruitItemsSet.Add(Type);
 		Item.buffType = BuffID.WellFed2;
+		Item.buffTime = 10 * 60 * 60;
 	}
 
 	public override void AddRecipes() => CreateRecipe().AddIngredient(ModContent.ItemType<OstrichEgg>())
