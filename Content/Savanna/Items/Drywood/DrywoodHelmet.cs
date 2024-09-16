f7b165c3444ca488b4e440b32afe2baa8cd3a78a
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-09-16 11:33:32 -0300

    Add Drywood set

---
+++ Content/Savanna/Items/Drywood/DrywoodHelmet.cs
@@ -0,0 +1,27 @@
+﻿namespace SpiritReforged.Content.Savanna.Items.Drywood;
+
+[AutoloadEquip(EquipType.Head)]
+public class DrywoodHelmet : ModItem
+{
+	public override void SetDefaults()
+	{
+		Item.width = 22;
+		Item.height = 20;
+		Item.value = Item.sellPrice(copper: 7);
+		Item.defense = 1;
+	}
+
+	public override bool IsArmorSet(Item head, Item body, Item legs) => (head.type, body.type, legs.type) ==
+		(ModContent.ItemType<DrywoodHelmet>(), ModContent.ItemType<DrywoodBreastplate>(), ModContent.ItemType<DrywoodGreaves>());
+
+	public override void UpdateArmorSet(Player player)
+	{
+		player.setBonus = Language.GetTextValue("Mods.SpiritReforged.SetBonuses.Drywood");
+
+		if (player.ZoneOverworldHeight || player.ZoneSkyHeight)
+			player.moveSpeed += .1f; //10% increase
+	}
+
+	public override void AddRecipes() => CreateRecipe().AddIngredient(ModContent.ItemType<Drywood>(), 20)
+			.AddTile(TileID.WorkBenches).Register();
+}
