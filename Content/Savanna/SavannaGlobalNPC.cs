5a00bbe316ac0386c65133f50bc301deda74dde2
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-09-20 21:02:22 -0300

    Savanna vulture spawning

--- Content/Savanna/SavannaGlobalNPC.cs
+++ Content/Savanna/SavannaGlobalNPC.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Terraria.DataStructures;
 
 namespace SpiritReforged.Content.Savanna;
 
@@ -11,6 +12,29 @@ public class SavannaGlobalNPC : GlobalNPC
 			var grassSeeds = items.Where(x => x != null && x.type == ItemID.GrassSeeds).FirstOrDefault();
 			if (grassSeeds != default)
 				grassSeeds.SetDefaults(ModContent.ItemType<Items.SavannaGrassSeeds>());
+		}
+	}
+
+	public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+	{
+		if (spawnInfo.Player.InModBiome<Biome.SavannaBiome>())
+		{
+			float odds = spawnInfo.Player.GetModPlayer<DustStorm.DustStormPlayer>().ZoneDustStorm ? .22f : .09f;
+			pool[NPCID.Vulture] = odds;
+		}
+	}
+
+	public override void OnSpawn(NPC npc, IEntitySource source)
+	{
+		if (npc.type == NPCID.Vulture && source is EntitySource_SpawnNPC)
+		{
+			//Move to an acacia treetop within 30 tiles when naturally spawned
+			var nearby = Tiles.AcaciaTree.AcaciaTree.Platforms.Where(x => x.Distance(npc.Center) < 16 * 30).OrderBy(x => x.Distance(npc.Center)).FirstOrDefault();
+			if (nearby != default)
+			{
+				npc.Center = nearby.Hitbox.ClosestPointInRect(npc.Center);
+				npc.netUpdate = true;
+			}
 		}
 	}
 }
