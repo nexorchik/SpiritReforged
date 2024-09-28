48dc3109784b47607eaf4aa200c9a7868ca49768
GabeHasWon <gabriel@guevarameyer.com>
2024-09-28 16:38:17 -0500

    start fish coves

---
+++ Common/WorldGeneration/Microtones/Passes/FishingAreaMicropass.cs
@@ -0,0 +1,41 @@
+﻿using Terraria.DataStructures;
+using Terraria.WorldBuilding;
+
+namespace SpiritReforged.Common.WorldGeneration.Microtones.Passes;
+
+internal class FishingAreaMicropass : Micropass
+{
+	public override string WorldGenName => "Adding fishing spots";
+
+	public override int GetWorldGenIndexInsert(List<GenPass> passes, ref bool afterIndex) => passes.FindIndex(genpass => genpass.Name.Equals("Sunflowers"));
+
+	public override void Run(GenerationProgress progress, Terraria.IO.GameConfiguration config)
+	{
+		int repeats = (int)(Main.maxTilesX / 4200f * 8);
+
+		for (int i = 0; i < repeats; ++i)
+		{
+			int subId = WorldGen.genRand.Next(5);
+			string subChar = WorldGen.genRand.NextBool() ? "a" : "b";
+			string structureName = "Assets/Structures/Coves/FishCove" + subId + subChar;
+			Point16 size = new();
+			StructureHelper.Generator.GetDimensions(structureName, ModContent.GetInstance<SpiritReforgedMod>(), ref size);
+			Point16 position;
+
+			do
+			{
+				position = new Point16(WorldGen.genRand.Next(60, Main.maxTilesX - 60), WorldGen.genRand.Next((int)Main.worldSurface, Main.maxTilesY - 400));
+			} while (CanPlaceStructureAt(size, position));
+
+			StructureTools.PlaceByOrigin(structureName, position, new(0));
+			GenVars.structures.AddProtectedStructure(new Rectangle(position.X, position.Y, size.X, size.Y), 6);
+		}
+	}
+
+	private static bool CanPlaceStructureAt(Point16 size, Point16 position)
+	{
+		bool open = GenVars.structures.CanPlace(new Rectangle(position.X, position.Y, size.X, size.Y), 10);
+		//bool hasAttachment = RecursivelyFindOpening(position, size);
+		return open;
+	}
+}
---
+++ Common/WorldGeneration/StructureTools.cs
@@ -0,0 +1,37 @@
+﻿using Terraria.DataStructures;
+
+namespace SpiritReforged.Common.WorldGeneration;
+
+/// <summary>
+/// Contains a few helper tools to use <see cref="StructureHelper.Generator"/>.
+/// </summary>
+internal static class StructureTools
+{
+	/// <summary>
+	/// Places a structure according to an origin. An <paramref name="origin"/> of (0.5f, 0.5f) would be centered, 
+	/// (0.5f, 1) would be centered on the X axis, and place from the bottom on the Y axis, and so on.
+	/// </summary>
+	/// <param name="structure">The path to the structure to place.</param>
+	/// <param name="position">Position of the structure.</param>
+	/// <param name="origin">Placement origin of the structure.</param>
+	/// <param name="mod">Mod to associate the structure with. Defaults to the Spirit Reforged instance.</param>
+	/// <param name="cullAbove">Whether to cull tiles directly above the spawned structure. This can be useful when spawning over trees or lare multitiles.</param>
+	public static void PlaceByOrigin(string structure, Point16 position, Vector2 origin, Mod mod = null, bool cullAbove = false)
+	{
+		mod ??= ModContent.GetInstance<SpiritReforgedMod>();
+		var dims = new Point16();
+		StructureHelper.Generator.GetDimensions(structure, mod, ref dims);
+		position = (position.ToVector2() - dims.ToVector2() * origin).ToPoint16();
+
+		if (cullAbove)
+			CullLine(position, dims);
+
+		StructureHelper.Generator.GenerateStructure(structure, position, mod);
+	}
+
+	private static void CullLine(Point16 position, Point16 dims)
+	{
+		for (int i = position.X; i < position.X + dims.X; ++i)
+			WorldGen.KillTile(i, position.Y - 1);
+	}
+}
