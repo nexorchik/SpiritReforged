3758d543bdf53fb7b0012c685c05c32f3f18fb11
PhoenixBladez <amanchala13@gmail.com>
2024-10-02 08:25:52 -0700

    Paintings

---
+++ Common/Misc/SpiritConditions.cs
@@ -0,0 +1,11 @@
+﻿namespace SpiritReforged.Common.Misc;
+
+internal class SpiritConditions
+{
+	// These InBiome properties use an anonymous method to wrap around the Zone checks.
+	// VS will mark this as needless, but removing the wrapping delegate will cause issues.
+	// This is because otherwise the condition used will capture an invalid LocalPlayer instance, causing a null ref.
+	// Fun!
+	// Ported Over From Spirit Classic
+	public static Condition InSavanna => new("Mods.SpiritReforged.Conditions.InSavanna", () => Main.LocalPlayer.InModBiome<Content.Savanna.Biome.SavannaBiome>());
+}
--- Content/Savanna/Tiles/Furniture/DrywoodBathtub.cs
+++ Content/Savanna/Tiles/Furniture/DrywoodBathtub.cs
@@ -3,12 +3,7 @@ using SpiritReforged.Common.TileCommon.FurnitureTiles;
 
 namespace SpiritReforged.Content.Savanna.Tiles.Furniture;
 
-public class DrywoodBathtub : BathtubTile, IAutoloadTileItem
+public class DrywoodBathtub : PaintingTile, IAutoloadTileItem
 {
-	public void SetItemDefaults(ModItem item) => item.Item.value = Item.sellPrice(copper: 60);
-
-	public void AddItemRecipes(ModItem item) => item.CreateRecipe()
-		.AddIngredient<Items.Drywood.Drywood>(14)
-		.AddTile(TileID.Sawmill)
-		.Register();
+	public void SetItemDefaults(ModItem item) => item.Item.value = Item.buyPrice(gold: 2);
 }
