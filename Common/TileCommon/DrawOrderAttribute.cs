aa5d7ce0c851bcf33c6db7ed94e9ac8dc869285c
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-10-03 20:19:41 -0300

    Add termite mounds + DrawOrderAttribute

---
+++ Common/TileCommon/DrawOrderAttribute.cs
@@ -0,0 +1,114 @@
+﻿using System.Linq;
+using Terraria.DataStructures;
+using Terraria.GameContent.Drawing;
+
+namespace SpiritReforged.Common.TileCommon;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class DrawOrderAttribute(DrawOrderAttribute.Layer layer, bool drawDefault = false) : Attribute
+{
+	public Layer layer = layer;
+	public bool drawDefault = drawDefault;
+
+	public enum Layer : byte //Draws above the given layer
+	{
+		NonSolid,
+		Solid,
+		OverPlayers,
+		Default
+	}
+}
+
+public class DrawOrderHandler : ILoadable
+{
+	public static readonly Dictionary<Point16, DrawOrderAttribute.Layer> specialDrawPoints = [];
+
+	/// <summary> Used in conjunction with <see cref="DrawOrderAttribute"/> to tell whether a tile is drawing as a result of the attribute. <br/>
+	/// Especially useful if <see cref="DrawOrderAttribute.drawDefault"/> is true. </summary>
+	internal static bool drawingInOrder = false;
+
+	public void Load(Mod mod)
+	{
+		static void Draw(Point16 p)
+			=> TileLoader.PreDraw(p.X, p.Y, Framing.GetTileSafely(p.X, p.Y).TileType, Main.spriteBatch);
+
+		On_TileDrawing.PreDrawTiles += ClearDrawPoints;
+		On_Main.DoDraw_Tiles_Solid += (On_Main.orig_DoDraw_Tiles_Solid orig, Main self) =>
+		{
+			orig(self);
+
+			Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.Transform);
+
+			drawingInOrder = true;
+			var above = specialDrawPoints.Where(x => x.Value is DrawOrderAttribute.Layer.Solid);
+			foreach (var set in above)
+				Draw(set.Key);
+
+			drawingInOrder = false;
+			Main.spriteBatch.End();
+		};
+
+		On_Main.DoDraw_Tiles_NonSolid += (On_Main.orig_DoDraw_Tiles_NonSolid orig, Main self) =>
+		{
+			orig(self);
+
+			drawingInOrder = true;
+			var above = specialDrawPoints.Where(x => x.Value is DrawOrderAttribute.Layer.NonSolid);
+			foreach (var set in above)
+				Draw(set.Key);
+
+			drawingInOrder = false;
+		};
+
+		On_Main.DrawPlayers_AfterProjectiles += (On_Main.orig_DrawPlayers_AfterProjectiles orig, Main self) =>
+		{
+			orig(self);
+
+			Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, Main.Rasterizer, null, Main.GameViewMatrix.TransformationMatrix);
+			
+			drawingInOrder = true;
+			var above = specialDrawPoints.Where(x => x.Value is DrawOrderAttribute.Layer.OverPlayers);
+			foreach (var set in above)
+				Draw(set.Key);
+
+			drawingInOrder = false;
+			Main.spriteBatch.End();
+		};
+	}
+
+	private void ClearDrawPoints(On_TileDrawing.orig_PreDrawTiles orig, TileDrawing self, bool solidLayer, bool forRenderTargets, bool intoRenderTargets)
+	{
+		orig(self, solidLayer, forRenderTargets, intoRenderTargets);
+
+		bool flag = intoRenderTargets || Lighting.UpdateEveryFrame;
+		if (!solidLayer && flag)
+			specialDrawPoints.Clear();
+	}
+
+	public void Unload() { }
+}
+
+public class DrawOrderGlobalTile : GlobalTile
+{
+	private static DrawOrderAttribute Tag(int type)
+	{
+		if (TileLoader.GetTile(type) is ModTile mTile)
+			return (DrawOrderAttribute)Attribute.GetCustomAttribute(mTile.GetType(), typeof(DrawOrderAttribute));
+
+		return null;
+	}
+
+	public override bool PreDraw(int i, int j, int type, SpriteBatch spriteBatch)
+	{
+		if (Tag(type) is not DrawOrderAttribute tag)
+			return true;
+
+		if (!DrawOrderHandler.drawingInOrder)
+		{
+			DrawOrderHandler.specialDrawPoints.Add(new Point16(i, j), tag.layer);
+			return tag.drawDefault;
+		}
+
+		return true;
+	}
+}
---
+++ Content/Savanna/Tiles/TermiteMound.cs
@@ -0,0 +1,135 @@
+﻿using SpiritReforged.Common.TileCommon;
+using Terraria.DataStructures;
+
+namespace SpiritReforged.Content.Savanna.Tiles;
+
+[DrawOrder(DrawOrderAttribute.Layer.Solid, true)]
+public class TermiteMoundLarge : ModTile
+{
+	public override string Texture => base.Texture.Replace("Large", string.Empty);
+
+	public override void SetStaticDefaults()
+	{
+		Main.tileSolid[Type] = false;
+		Main.tileFrameImportant[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+		TileObjectData.newTile.Height = 5;
+		TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 16, 18];
+		TileObjectData.newTile.Origin = new(2, 4);
+		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, 3, 0);
+		TileObjectData.newTile.AnchorValidTiles = [ModContent.TileType<SavannaGrass>()];
+		TileObjectData.newTile.StyleHorizontal = true;
+		TileObjectData.addTile(Type);
+
+		AddMapEntry(new Color(100, 92, 50));
+		DustType = DustID.Dirt;
+	}
+
+	public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
+
+	public override void KillMultiTile(int i, int j, int frameX, int frameY)
+	{
+		if (Main.netMode == NetmodeID.MultiplayerClient)
+			return;
+
+		for (int x = 0; x < 5; x++)
+		{
+			var position = new Vector2(i, j) * 16 + Main.rand.NextVector2FromRectangle(new Rectangle(0, 0, 3 * 16, 5 * 16));
+			var velocity = (Vector2.UnitY * -Main.rand.NextFloat(6f)).RotatedByRandom(2f);
+
+			var npc = NPC.NewNPCDirect(new EntitySource_TileBreak(i, j), position, ModContent.NPCType<NPCs.Termite.Termite>());
+			npc.velocity = velocity;
+			npc.netUpdate = true;
+		}
+	}
+
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		if (!DrawOrderHandler.drawingInOrder)
+			return true;
+
+		var tile = Framing.GetTileSafely(i, j);
+		if (tile.TileFrameY == 0)
+		{
+			var texture = TextureAssets.Tile[Type].Value;
+			var frame = new Point(18 * 7, 18 * (2 + tile.TileFrameX / 18));
+			var source = new Rectangle(frame.X, frame.Y, 16, 16);
+			var position = new Vector2(i, j + 5) * 16 - Main.screenPosition - new Vector2(0, 2);
+
+			spriteBatch.Draw(texture, position, source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+		}
+
+		return false;
+	}
+}
+
+[DrawOrder(DrawOrderAttribute.Layer.Solid, true)]
+public class TermiteMoundSmall : ModTile
+{
+	public override string Texture => base.Texture.Replace("Small", string.Empty);
+
+	public override void SetStaticDefaults()
+	{
+		Main.tileSolid[Type] = false;
+		Main.tileFrameImportant[Type] = true;
+
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+		TileObjectData.newTile.Height = 4;
+		TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 18];
+		TileObjectData.newTile.Origin = new(1, 3);
+		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, 2, 0);
+		TileObjectData.newTile.AnchorValidTiles = [ModContent.TileType<SavannaGrass>()];
+		TileObjectData.newTile.StyleHorizontal = true;
+		TileObjectData.newTile.RandomStyleRange = 2;
+		TileObjectData.addTile(Type);
+
+		AddMapEntry(new Color(100, 92, 50));
+		DustType = DustID.Dirt;
+	}
+
+	public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
+
+	public override void KillMultiTile(int i, int j, int frameX, int frameY)
+	{
+		if (Main.netMode == NetmodeID.MultiplayerClient)
+			return;
+
+		for (int x = 0; x < 3; x++)
+		{
+			var position = new Vector2(i, j) * 16 + Main.rand.NextVector2FromRectangle(new Rectangle(0, 0, 2 * 16, 4 * 16));
+			var velocity = (Vector2.UnitY * -Main.rand.NextFloat(6f)).RotatedByRandom(2f);
+
+			var npc = NPC.NewNPCDirect(new EntitySource_TileBreak(i, j), position, ModContent.NPCType<NPCs.Termite.Termite>());
+			npc.velocity = velocity;
+			npc.netUpdate = true;
+		}
+	}
+
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		var tile = Framing.GetTileSafely(i, j);
+		var texture = TextureAssets.Tile[Type].Value;
+
+		if (!DrawOrderHandler.drawingInOrder)
+		{
+			var frame = new Point(tile.TileFrameX + 18 * 3, tile.TileFrameY + 18);
+			var source = new Rectangle(frame.X, frame.Y, 16, tile.TileFrameY == 18 * 3 ? 18 : 16);
+
+			var zero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange, Main.offScreenRange);
+			var position = new Vector2(i, j) * 16 - Main.screenPosition + zero;
+
+			spriteBatch.Draw(texture, position, source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+		}
+		else if (tile.TileFrameY == 0)
+		{
+			var frame = new Point(18 * 7, 18 * (2 + tile.TileFrameX % (18 * 2) / 18));
+			var source = new Rectangle(frame.X, frame.Y, 16, 16);
+			var position = new Vector2(i, j + 4) * 16 - Main.screenPosition - new Vector2(0, 2);
+
+			spriteBatch.Draw(texture, position, source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+		}
+
+		return false;
+	}
+}
