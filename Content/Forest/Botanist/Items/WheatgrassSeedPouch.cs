a12bb72f5aec1d5567521d7e59d5824aaf212afb
GabeHasWon <gabriel@guevarameyer.com>
2024-10-13 12:28:04 -0500

    finish wheatgrass seeds

--- Content/Forest/Botanist/Items/WheatgrassSeedPouch.cs
+++ Content/Forest/Botanist/Items/WheatgrassSeedPouch.cs
@@ -1,22 +1,32 @@
+using SpiritReforged.Common.NPCCommon;
+using Terraria.DataStructures;
+
 namespace SpiritReforged.Content.Forest.Botanist.Items;
 
-[AutoloadEquip(EquipType.Legs)]
 public class WheatgrassSeedPouch : ModItem
 {
+	public override void SetStaticDefaults() => NPCShopHelper.AddEntry(new NPCShopHelper.ConditionalEntry((shop) => shop.NpcType == NPCID.Merchant, 
+		new NPCShop.Entry(ModContent.ItemType<WheatgrassSeedPouch>(), Condition.DownedEyeOfCthulhu)));
+
 	public override void SetDefaults()
 	{
-		Item.width = 30;
-		Item.height = 20;
-		Item.value = Item.sellPrice(0, 0, 10, 0);
-		Item.rare = ItemRarityID.White;
-		Item.defense = 1;
+		Item.width = 26;
+		Item.height = 26;
+		Item.value = Item.sellPrice(0, 0, 0, 5);
+		Item.rare = ItemRarityID.Blue;
+		Item.shoot = ModContent.ProjectileType<WheatgrassSeedProjectile>();
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.shootSpeed = 8;
+		Item.useTime = Item.useAnimation = 25;
+		Item.consumable = true;
+		Item.maxStack = Item.CommonMaxStack;
 	}
 
-	public override void UpdateEquip(Player player) => player.moveSpeed += 0.1f;
+	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+	{
+		for (int i = 0; i < 6; ++i)
+			Projectile.NewProjectile(source, position, velocity.RotatedByRandom(0.4f) * Main.rand.NextFloat(0.5f, 1f), type, 0, knockback, player.whoAmI);	
 
-	public override void AddRecipes() => CreateRecipe()
-			.AddIngredient(ItemID.Sunflower, 1)
-			.AddIngredient(ItemID.FallenStar, 1)
-			.AddTile(TileID.WorkBenches)
-			.Register();
+		return false;
+	}
 }
