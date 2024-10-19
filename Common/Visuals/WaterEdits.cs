bddcd3d7ee58f25942f2752a9628c7efeff5f049
GabeHasWon <gabriel@guevarameyer.com>
2024-10-18 21:53:33 -0500

    most of water transparency, needs drawblack and water darkness fixes

---
+++ Common/Visuals/WaterEdits.cs
@@ -0,0 +1,98 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using Terraria.GameContent.Drawing;
+using Terraria.GameContent.Liquid;
+using Terraria.Graphics;
+
+namespace SpiritReforged.Common.Visuals;
+
+internal class WaterEdits : ModSystem
+{
+	public bool DrawingLiquid = false;
+
+	public override void Load()
+	{
+		On_LiquidRenderer.DrawNormalLiquids += CheckLiquid;
+		On_Lighting.GetCornerColors += HijackLiquidSlopeColoring;
+		On_TileDrawing.DrawPartialLiquid += FixSlopes;
+
+		IL_Main.DrawBlack += HijackDrawBlack;
+		On_Main.DrawBlack += On_Main_DrawBlack;
+	}
+
+	private void On_Main_DrawBlack(On_Main.orig_DrawBlack orig, Main self, bool force) => orig(self, force);
+
+	private void HijackDrawBlack(ILContext il)
+	{
+		ILCursor c = new(il);
+
+		if (!c.TryGotoNext(x => x.MatchLdsfld<Main>(nameof(Main.spriteBatch))))
+			return;
+
+		if (!c.TryGotoPrev(x => x.MatchLdloc(out int _)))
+			return;
+
+		if (!c.TryGotoPrev(x => x.MatchLdloc(out int _)))
+			return;
+
+		c.Index++;
+
+		c.Emit(OpCodes.Pop); // Remove old local (13) value off stack, modify them in ModifyEdges, then push 13 on the stack again
+		c.Emit(OpCodes.Ldloca_S, (byte)13);
+		c.Emit(OpCodes.Ldloc_S, (byte)11);
+		c.Emit(OpCodes.Ldloca_S, (byte)14);
+		c.EmitDelegate(ModifyEdges);
+		c.Emit(OpCodes.Ldloc_S, (byte)13);
+	}
+
+	public static void ModifyEdges(ref int left, int y, ref int drawPosX)
+	{
+		if (left - drawPosX > 0) // && Main.tile[left, y].Slope != SlopeType.Solid)
+			left--;
+
+		if (left - drawPosX > 0)
+			drawPosX++;
+	}
+
+	private void FixSlopes(On_TileDrawing.orig_DrawPartialLiquid orig, TileDrawing self, bool behind, Tile tileCache, ref Vector2 pos,
+		ref Rectangle size, int liquidType, ref VertexColors colors)
+	{
+		DrawingLiquid = true;
+
+		ModifyVertexColors(ref colors, 0.8f);
+
+		orig(self, behind, tileCache, ref pos, ref size, liquidType, ref colors);
+		DrawingLiquid = false;
+	}
+
+	private void HijackLiquidSlopeColoring(On_Lighting.orig_GetCornerColors orig, int centerX, int centerY, out VertexColors vertices, float scale)
+	{
+		orig(centerX, centerY, out vertices, scale);
+
+		if (DrawingLiquid)
+			ModifyVertexColors(ref vertices);
+	}
+
+	private static void ModifyVertexColors(ref VertexColors vertices, float opacity = 1f)
+	{
+		ModifyColor(ref vertices.TopLeftColor, opacity);
+		ModifyColor(ref vertices.TopRightColor, opacity);
+		ModifyColor(ref vertices.BottomLeftColor, opacity);
+		ModifyColor(ref vertices.BottomRightColor, opacity);
+	}
+
+	private static void ModifyColor(ref Color color, float opacity = 1f)
+	{
+		color *= Math.Abs(color.B / 255f) * 0.25f + 0.75f;
+		color = Color.Lerp(color, new Color(8, 8, 80) * 0.75f * opacity, 1 - Luminance(color));
+	}
+
+	private static float Luminance(Color color) => 0.299f * color.R / 255f + 0.587f * color.G / 255f + 0.114f * color.B / 255f;
+
+	private void CheckLiquid(On_LiquidRenderer.orig_DrawNormalLiquids orig, LiquidRenderer self, SpriteBatch batch, Vector2 off, int style, float alpha, bool bg)
+	{
+		DrawingLiquid = true;
+		orig(self, batch, off, style, alpha, bg);
+		DrawingLiquid = false;
+	}
+}
---
+++ Content/Ocean/DeepOceanScene.cs
@@ -0,0 +1,12 @@
+﻿namespace SpiritReforged.Content.Ocean;
+
+internal class DeepOceanScene : ModSceneEffect
+{
+	public override int Music => MusicLoader.GetMusicSlot(Mod, "Assets/Music/DeepOcean");
+	public override SceneEffectPriority Priority => SceneEffectPriority.BiomeHigh;
+	public override ModSurfaceBackgroundStyle SurfaceBackgroundStyle => Mod.Find<ModSurfaceBackgroundStyle>("DeepOceanBackgroundStyle");
+	//public override ModWaterStyle WaterStyle => WaterStyleID.Purity;
+
+	public override bool IsSceneEffectActive(Player player) => player.ZoneBeach && player.GetModPlayer<OceanPlayer>().Submerged(30) && NotInDepths(player);
+	private static bool NotInDepths(Player plr) => !ModLoader.TryGetMod("ThoriumMod", out Mod thor) || thor.Call("GetZoneAquaticDepths", plr) is bool depths && !depths;
+}
