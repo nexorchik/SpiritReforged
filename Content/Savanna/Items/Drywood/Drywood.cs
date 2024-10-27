3c7790177fc90ecc56a5aea8d1e51d50542c86a3
GabeHasWon <gabriel@guevarameyer.com>
2024-10-27 10:43:44 -0500

    add drywood tile

--- Content/Savanna/Items/Drywood/Drywood.cs
+++ Content/Savanna/Items/Drywood/Drywood.cs
@@ -1,11 +1,13 @@
-﻿namespace SpiritReforged.Content.Savanna.Items.Drywood;
+﻿using SpiritReforged.Content.Savanna.Tiles;
+
+namespace SpiritReforged.Content.Savanna.Items.Drywood;
 
 public class Drywood : ModItem
 {
 	public override void SetDefaults()
 	{
+		Item.DefaultToPlaceableTile(ModContent.TileType<DrywoodTile>());
 		Item.width = 14;
 		Item.height = 14;
-		Item.maxStack = Item.CommonMaxStack;
 	}
 }
