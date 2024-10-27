85415a440004a3cbd37548a5e04f8fa7bc002408
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-10-27 18:43:46 -0300

    Mineral Slag Extractinator + recipe uses

--- Content/Ocean/Items/MineralSlag.cs
+++ Content/Ocean/Items/MineralSlag.cs
@@ -1,15 +1,25 @@
 using SpiritReforged.Common.Particle;
 using SpiritReforged.Content.Particles;
+using Terraria.Utilities;
 
 namespace SpiritReforged.Content.Ocean.Items;
 
 public class MineralSlag : ModItem
 {
+	private struct ItemData(int itemType, int stack = 1) //Extractinator use
+	{
+		public int itemType = itemType;
+		public int stack = stack;
+	}
+
 	protected const int frameCount = 5;
 	protected int subID = -1; //Controls the in-world sprite for this item
 
-	public override void SetStaticDefaults() 
-		=> Main.RegisterItemAnimation(Type, new Terraria.DataStructures.DrawAnimationVertical(2, frameCount) { NotActuallyAnimating = true });
+	public override void SetStaticDefaults()
+	{
+		Main.RegisterItemAnimation(Type, new Terraria.DataStructures.DrawAnimationVertical(2, frameCount) { NotActuallyAnimating = true });
+		ItemID.Sets.ExtractinatorMode[Type] = Type;
+	}
 
 	public override void SetDefaults()
 	{
@@ -17,6 +27,11 @@ public class MineralSlag : ModItem
 		Item.value = 100;
 		Item.maxStack = Item.CommonMaxStack;
 		Item.rare = ItemRarityID.Blue;
+		Item.useTime = 10;
+		Item.useAnimation = 15;
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.autoReuse = true;
+		Item.consumable = true;
 	}
 
 	public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
@@ -35,6 +50,28 @@ public class MineralSlag : ModItem
 	{
 		subID = -1;
 		return true;
+	}
+
+	public override void ExtractinatorUse(int extractinatorBlockType, ref int resultType, ref int resultStack)
+	{
+		var choice = new WeightedRandom<ItemData>();
+		choice.Add(new ItemData(ItemID.GoldCoin), .09f);
+		choice.Add(new ItemData(ItemID.SilverCoin, Main.rand.Next(3) + 1), .7f);
+		choice.Add(new ItemData(ItemID.CopperCoin, Main.rand.Next(40) + 1), 1.25f);
+
+		choice.Add(new ItemData(ItemID.CopperOre, Main.rand.Next(3) + 1));
+		choice.Add(new ItemData(ItemID.TinOre, Main.rand.Next(3) + 1));
+		choice.Add(new ItemData(ItemID.IronOre, Main.rand.Next(3) + 1));
+		choice.Add(new ItemData(ItemID.LeadOre, Main.rand.Next(3) + 1));
+		choice.Add(new ItemData(ItemID.SilverOre, Main.rand.Next(3) + 1), .8f);
+		choice.Add(new ItemData(ItemID.TungstenOre, Main.rand.Next(3) + 1), .8f);
+		choice.Add(new ItemData(ItemID.GoldOre, Main.rand.Next(3) + 1), .5f);
+		choice.Add(new ItemData(ItemID.PlatinumOre, Main.rand.Next(3) + 1), .5f);
+
+		choice.Add(new ItemData(ItemID.AmberMosquito), .01f);
+
+		resultType = ((ItemData)choice).itemType;
+		resultStack = ((ItemData)choice).stack;
 	}
 }
 
