cfb945f326ff2e19f0113a8952f6597544b6ad8e
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-12-04 12:03:40 -0400

    Savanna fountain fixes

--- Content/Savanna/Tiles/SavannaFountain.cs
+++ Content/Savanna/Tiles/SavannaFountain.cs
@@ -2,12 +2,16 @@ using SpiritReforged.Common.NPCCommon;
 using SpiritReforged.Common.PlayerCommon;
 using SpiritReforged.Common.TileCommon;
 using SpiritReforged.Content.Savanna.Biome;
+using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.GameContent.ObjectInteractions;
 
 namespace SpiritReforged.Content.Savanna.Tiles;
 
 public class SavannaFountain : ModTile, IAutoloadTileItem
 {
+	private const int FrameHeight = 72;
+
 	void IAutoloadTileItem.SetItemDefaults(ModItem item) => item.Item.SetShopValues(ItemRarityColor.Blue1, Item.buyPrice(0, 4, 0, 0));
 
 	public override void SetStaticDefaults()
@@ -20,12 +24,15 @@ public class SavannaFountain : ModTile, IAutoloadTileItem
 		TileID.Sets.HasOutlines[Type] = true;
 
 		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+		TileObjectData.newTile.Origin = new Point16(1, 3);
 		TileObjectData.newTile.Height = 4;
 		TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 16];
 		TileObjectData.newTile.DrawYOffset = 2;
 		TileObjectData.addTile(Type);
 
 		DustType = DustID.Stone;
+		AnimationFrameHeight = FrameHeight;
+		AdjTiles = [TileID.WaterFountain];
 
 		int itemId = Mod.Find<ModItem>("SavannaFountainItem").Type;
 		RegisterItemDrop(itemId);
@@ -36,14 +43,24 @@ public class SavannaFountain : ModTile, IAutoloadTileItem
 	public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) => true;
 	public override void AnimateTile(ref int frame, ref int frameCounter) => frame = Main.tileFrame[TileID.WaterFountain];
 
+	public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset)
+	{
+		var tile = Main.tile[i, j];
+		if (tile.TileFrameY >= FrameHeight)
+			frameYOffset = Main.tileFrame[type] * FrameHeight;
+		else
+			frameYOffset = 0; //Don't animate when turned off
+	}
+
 	public override void NearbyEffects(int i, int j, bool closer)
 	{
-		if (Main.tile[i, j].TileFrameY >= 72)
+		if (Main.tile[i, j].TileFrameY >= FrameHeight)
 			Main.LocalPlayer.GetModPlayer<FountainPlayer>().SetFountain<SavannaWaterStyle>();
 	}
 
 	public override bool RightClick(int i, int j)
 	{
+		SoundEngine.PlaySound(SoundID.Mech, new Vector2(i * 16, j * 16));
 		ToggleTile(i, j);
 		return true;
 	}
@@ -52,29 +69,27 @@ public class SavannaFountain : ModTile, IAutoloadTileItem
 
 	private void ToggleTile(int i, int j)
 	{
-		Tile anchor = Framing.GetTileSafely(i, j);
-		int x = i - anchor.TileFrameX / 18 % 2;
-		int y = j - anchor.TileFrameY / 18 % 4;
+		TileExtensions.GetTopLeft(ref i, ref j);
 
-		for (int l = x; l < x + 2; l++)
+		for (int x = i; x < i + 2; x++)
 		{
-			for (int m = y; m < y + 4; m++)
+			for (int y = j; y < j + 4; y++)
 			{
-				Tile tile = Framing.GetTileSafely(l, m);
+				Tile tile = Framing.GetTileSafely(x, y);
 
 				if (tile.HasTile && tile.TileType == Type)
 				{
-					if (tile.TileFrameY < 72)
-						tile.TileFrameY += 72;
+					if (tile.TileFrameY < FrameHeight)
+						tile.TileFrameY += FrameHeight;
 					else
-						tile.TileFrameY -= 72;
+						tile.TileFrameY -= FrameHeight;
 				}
 
-				Wiring.SkipWire(l, m);
+				Wiring.SkipWire(x, y);
 			}
 		}
 
 		if (Main.netMode != NetmodeID.SinglePlayer)
-			NetMessage.SendTileSquare(-1, x, y, 2, 4);
+			NetMessage.SendTileSquare(-1, i, j, 2, 4);
 	}
 }
