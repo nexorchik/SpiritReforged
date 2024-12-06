e5a8ef6318349cb307580dcf247723e86f1e13b2
PhoenixBladez <amanchala13@gmail.com>
2024-12-05 23:33:54 -0800

    Railgun

---
+++ Content/Underground/Items/Railgun/ZiplinePlayer.cs
@@ -0,0 +1,46 @@
+namespace SpiritReforged.Content.Underground.Items.Railgun;
+
+public class ZiplinePlayer : ModPlayer
+{
+	public Vector2 directionUnit = Vector2.Zero;
+	private int cooldown;
+
+	public bool OnZipline => directionUnit != Vector2.Zero;
+
+	public override void PreUpdateMovement()
+	{
+		if (OnZipline && cooldown <= 0) //The player is on a "zipline"
+		{
+			bool cancel = Player.controlJump || Player.controlDown || Player.grappling[0] > -1;
+
+			if (cancel)
+			{
+				cooldown = 30;
+				if (Player.controlJump)
+				{
+					Player.velocity.Y -= Player.jumpSpeed * Player.gravDir;
+					Player.jump = Player.jumpHeight;
+				}
+			}
+			else
+			{
+				Vector2 adjustment = Vector2.UnitY * -0.1f; //This keeps the player in position
+				float magnitude = Math.Sign(Player.velocity.X) * (7f * Player.moveSpeed);
+				Player.velocity = directionUnit * magnitude + adjustment;
+				Player.fallStart = (int)Player.position.Y / 16;
+
+				if (Player.velocity.Length() > .5f)
+					Dust.NewDustPerfect(Player.Center + Vector2.UnitY * (Player.height / 2) * Player.gravDir, DustID.Torch, new Vector2(-Player.velocity.X * Main.rand.NextFloat(), -Main.rand.NextFloat()), 0, default, .75f);
+			}
+		}
+
+		cooldown = Math.Max(0, --cooldown);
+		directionUnit = Vector2.Zero;
+	}
+
+	public override void PreUpdate()
+	{
+		if (OnZipline) 
+			Player.justJumped = true;
+	}
+}
---
+++ Content/Underground/UndergroundGeneration.cs
@@ -0,0 +1,9 @@
+﻿using SpiritReforged.Content.Underground.Items.Railgun;
+using static SpiritReforged.Common.Misc.ChestPoolUtils;
+
+namespace SpiritReforged.Content.Underground;
+
+public class UndergroundGeneration : ModSystem
+{
+	public override void PostWorldGen() => AddToVanillaChest(new ChestInfo(new int[] { ModContent.ItemType<ZiplineGun>() }, 1, 0.25f), goldChests, 1);
+}
