a555440d3ee835e61bf49e6e625a5be6e71ba451
PhoenixBladez <amanchala13@gmail.com>
2024-12-07 21:06:07 -0800

    Revert "Frostbite Port"
    This reverts commit 47ac496c1a259bfe95045985ad0167ff70912aaa.

--- Content/Snow/SnowGeneration.cs
+++
@@ -1,9 +0,0 @@
-﻿using SpiritReforged.Common.Misc;
-using SpiritReforged.Content.Snow.Frostbite; 
-
-namespace SpiritReforged.Content.Snow;
-
-public class SnowGeneration : ModSystem
-{
-	public override void PostWorldGen() => ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<FrostbiteItem>() }, 1, 0.25f), ChestPoolUtils.goldChests, 1);
-}
