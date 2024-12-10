d9ea497b21e078f26af24ec52984d086ff8f241e
PhoenixBladez <amanchala13@gmail.com>
2024-12-09 22:54:38 -0800

    Savanna spawning changes: Remove regular overworld spawns, add bird

--- Content/Savanna/Tiles/AcaciaTree/AcaciaTree.cs
+++ Content/Savanna/Tiles/AcaciaTree/AcaciaTree.cs
@@ -25,6 +25,7 @@ public class AcaciaTree : CustomTree
 
 	public override void PostSetStaticDefaults()
 	{
+
 		TileObjectData.newTile.AnchorValidTiles = [ModContent.TileType<SavannaGrass>()];
 
 		AddMapEntry(new Color(120, 80, 75));
