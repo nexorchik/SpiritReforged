5e276da261199f1668f70b4c8abb546cf2807803
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-12-12 19:32:59 -0400

    ButterflyStump polish

--- Content/Forest/ButterflyStaff/ButterflyStump.cs
+++ Content/Forest/ButterflyStaff/ButterflyStump.cs
@@ -1,15 +1,20 @@
 using SpiritReforged.Common.TileCommon;
 using Terraria.DataStructures;
+using Terraria.GameContent.Drawing;
 using Terraria.GameContent.ObjectInteractions;
 
 namespace SpiritReforged.Content.Forest.ButterflyStaff;
 
 public class ButterflyStump : ModTile
 {
+	private static Asset<Texture2D> glowTexture;
 	private const int FrameHeight = 18 * 4;
 
 	private static int ItemType => ModContent.ItemType<ButterflyStaff>();
 	private static bool HasItem(int i, int j) => Framing.GetTileSafely(i, j).TileFrameY < FrameHeight;
+	private static bool TopHalf(int i, int j) => Framing.GetTileSafely(i, j).TileFrameY % FrameHeight < 18 * 2;
+
+	public override void Load() => glowTexture = ModContent.Request<Texture2D>(Texture + "_Glow");
 
 	public override void SetStaticDefaults()
 	{
@@ -36,7 +41,10 @@ public class ButterflyStump : ModTile
 		DustType = DustID.WoodFurniture;
 	}
 
-	public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) => true;
+	public override bool CreateDust(int i, int j, ref int type) => !TopHalf(i, j);
+	public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) => HasItem(i, j);
+	public override bool CanKillTile(int i, int j, ref bool blockDamaged) => !TopHalf(i, j) || HasItem(i, j);
+	public override bool CanDrop(int i, int j) => HasItem(i, j);
 
 	public override void MouseOver(int i, int j)
 	{
@@ -72,8 +80,6 @@ public class ButterflyStump : ModTile
 		return false;
 	}
 
-	public override bool CanDrop(int i, int j) => HasItem(i, j);
-
 	public override void AnimateTile(ref int frame, ref int frameCounter)
 	{
 		if (++frameCounter >= 4)
@@ -96,7 +102,27 @@ public class ButterflyStump : ModTile
 		if (!HasItem(i, j))
 			return;
 
-		var color = Color.Magenta * .005f;
-		(r, g, b) = (color.R, color.G, color.B);
+		var color = new Vector3(255, 125, 255) * .001f;
+		(r, g, b) = (color.X, color.Y, color.Z);
+	}
+
+	public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		var tile = Main.tile[i, j];
+		if (!TileDrawing.IsVisible(tile))
+			return;
+
+		if (HasItem(i, j))
+		{
+			var zero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange, Main.offScreenRange);
+			int addFrameX = 0, addFrameY = 0;
+
+			TileLoader.SetAnimationFrame(Type, i, j, ref addFrameX, ref addFrameY);
+
+			var source = new Rectangle(tile.TileFrameX, tile.TileFrameY + addFrameY, 16, 16);
+			var position = new Vector2(i, j) * 16 - Main.screenPosition + zero + new Vector2(0, 2);
+
+			spriteBatch.Draw(glowTexture.Value, position, source, (Color.White with { A = 0 }) * .2f, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+		}
 	}
 }
