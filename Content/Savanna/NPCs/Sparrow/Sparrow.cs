211754f231a962e13a74645d0a025b318f27591d
PhoenixBladez <amanchala13@gmail.com>
2024-12-15 00:42:50 -0800

    Sparrow + SavannaRecipes FIle

---
+++ Content/Savanna/NPCs/Sparrow/Sparrow.cs
@@ -0,0 +1,29 @@
+using SpiritReforged.Content.Savanna.Biome;
+using SpiritReforged.Content.Savanna.DustStorm;
+using Terraria.GameContent.Bestiary;
+
+namespace SpiritReforged.Content.Savanna.NPCs.Sparrow;
+
+[AutoloadCritter]
+public class Sparrow : ModNPC
+{
+	public override void SetStaticDefaults() => Main.npcFrameCount[Type] = 5;
+
+	public override void SetDefaults()
+	{
+		NPC.CloneDefaults(NPCID.Bird);
+		AIType = NPCID.Bird;
+		AnimationType = NPCID.Bird;
+	}
+
+	//public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) => bestiaryEntry.AddInfo(this, "Savanna");
+
+	public override void HitEffect(NPC.HitInfo hit)
+	{
+		for (int k = 0; k < 8; k++)
+			Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.Blood, 2.5f * hit.HitDirection, -2.5f, 0, default, .9f);
+
+		if (NPC.life <= 0 && !Main.dedServ)
+			Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SparrowGore").Type, 1f);
+	}
+}
