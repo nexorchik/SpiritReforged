1ed3bbc9b851bd5522daadb17bf44b8b470952cc
PhoenixBladez <amanchala13@gmail.com>
2024-12-15 16:37:27 -0800

    Added OceanRecipes and a Gravel recipe

---
+++ Content/Ocean/OceanRecipes.cs
@@ -0,0 +1,14 @@
+﻿using SpiritReforged.Content.Ocean.Items;
+
+namespace SpiritReforged.Content.Ocean;
+
+public class OceanRecipes : ModSystem
+{
+	public override void AddRecipes()
+	{
+		Recipe.Create(Mod.Find<ModItem>("GravelItem").Type, 10)
+			.AddIngredient(ModContent.ItemType<MineralSlag>())
+			.AddTile(TileID.Bottles)
+			.Register();
+	}
+}
