c83a1ec93cdda703a1b6e0e6477521f24c641c3d
Abhay M <amanchala13@gmail.com>
2024-12-24 17:06:18 -0800

    Continue Leather Cloak

--- Content/Vanilla/LeatherCloak/LeatherCloak.cs
+++ Content/Vanilla/LeatherCloak/LeatherCloak.cs
@@ -1,15 +1,35 @@
 ﻿using SpiritReforged.Common.ItemCommon;
+using SpiritReforged.Common.PlayerCommon;
+using SpiritReforged.Content.Desert.GildedScarab;
 
-namespace SpiritReforged.Content.Desert.GildedScarab;
+namespace SpiritReforged.Content.Vanilla.LeatherCloak;
 
-internal class GildedScarab : AccessoryItem
+internal class LeatherCloak : AccessoryItem
 {
 	public override void SetDefaults()
 	{
-		Item.width = 34;
-		Item.height = 32;
+		Item.width = 26;
+		Item.height = 26;
 		Item.rare = ItemRarityID.Blue;
 		Item.accessory = true;
-		Item.value = Item.sellPrice(0, 1, 0, 0);
+		Item.value = Item.sellPrice(0, 0, 5, 0);
+	}
+
+	public override void UpdateAccessory(Player player, bool hideVisual)
+	{
+		if (player.ZoneDesert)
+			player.buffImmune[BuffID.WindPushed] = true;
+	}
+	private class LeatherCloakPlayer : ModPlayer
+	{
+		public override void PostUpdateRunSpeeds()
+		{
+			if (Player.HasAccessory<LeatherCloak>())
+			{
+				Player.runAcceleration *= 1.15f;
+				Player.maxRunSpeed += 0.1f;
+				Player.accRunSpeed += 0.05f;
+			}
+		}
 	}
 }
