acb13ece83729f79e4bd9cb7def607a9b77aea2a
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2024-12-31 15:38:15 -0400

    Update StargrassVine physics + GlowmaskTile changes

--- Common/Visuals/Glowmasks/GlowmaskTile.cs
+++ Common/Visuals/Glowmasks/GlowmaskTile.cs
@@ -1,17 +1,61 @@
-﻿using SpiritReforged.Common.TileCommon;
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System.Linq;
+using Terraria.GameContent.Drawing;
 
 namespace SpiritReforged.Common.Visuals.Glowmasks;
 
-internal class GlowmaskTile : GlobalTile
+internal class GlowmaskTile : ILoadable
 {
 	public static Dictionary<int, GlowmaskInfo> TileIdToGlowmask = [];
 
-	public override void PostDraw(int i, int j, int type, SpriteBatch spriteBatch)
+	public void Load(Mod mod) => IL_TileDrawing.GetTileDrawData += InjectGlowmaskData;
+
+	private void InjectGlowmaskData(ILContext il)
 	{
-		if (TileIdToGlowmask.TryGetValue(type, out var glow) && glow.DrawAutomatically)
+		ILCursor c = new(il);
+
+		c.Index = c.Instrs.Count - 1; //Move to the end
+
+		var p_typeCache = c.Method.Parameters.Where(x => x.Name == "typeCache").FirstOrDefault();
+		var p_glowTexture = c.Method.Parameters.Where(x => x.Name == "glowTexture").FirstOrDefault();
+		var p_glowColor = c.Method.Parameters.Where(x => x.Name == "glowColor").FirstOrDefault();
+		var p_glowSourceRect = c.Method.Parameters.Where(x => x.Name == "glowSourceRect").FirstOrDefault();
+
+		if (p_typeCache == default || p_glowTexture == default || p_glowColor == default || p_glowSourceRect == default)
 		{
-			Vector2 pos = TileExtensions.DrawPosition(i, j);
-			TileExtensions.DrawSloped(i, j, glow.Glowmask.Value, glow.GetDrawColor(new Point(i, j)), Vector2.Zero);
+			SpiritReforgedMod.Instance.Logger.Info($"IL edit '{nameof(InjectGlowmaskData)}' failed; all required parameters not found.");
+			return;
+		}
+
+		c.Emit(OpCodes.Ldarg_S, p_typeCache); //The tile type
+		c.EmitLdarg1(); //i
+		c.EmitLdarg2(); //j
+
+		c.Emit(OpCodes.Ldarg_S, p_glowTexture); //Glowmask
+		c.Emit(OpCodes.Ldarg_S, p_glowColor); //Glow color
+		c.Emit(OpCodes.Ldarg_S, p_glowSourceRect); //Glow frame
+
+		c.EmitDelegate(ModifyData);
+	}
+
+	private void ModifyData(int typeCache, int i, int j, ref Texture2D glowTexture, ref Color glowColor, ref Rectangle glowSourceRect)
+	{
+		if (TileIdToGlowmask.TryGetValue(typeCache, out GlowmaskInfo value) && value.DrawAutomatically)
+		{
+			glowTexture = TileIdToGlowmask[typeCache].Glowmask.Value;
+			glowColor = TileIdToGlowmask[typeCache].GetDrawColor(new Point(i, j));
+
+			var tile = Main.tile[i, j];
+			int addFrameX = 0;
+			int addFrameY = 0;
+
+			TileLoader.SetAnimationFrame(typeCache, i, j, ref addFrameX, ref addFrameY);
+			var source = new Rectangle(tile.TileFrameX, tile.TileFrameY + addFrameY, 16, 16);
+
+			glowSourceRect = source;
 		}
 	}
+
+	public void Unload() { }
 }
--- Content/Forest/Stargrass/Tiles/StargrassVine.cs
+++ Content/Forest/Stargrass/Tiles/StargrassVine.cs
@@ -1,11 +1,10 @@
-﻿using SpiritReforged.Common.TileCommon;
+﻿using SpiritReforged.Common.Visuals.Glowmasks;
 
 namespace SpiritReforged.Content.Forest.Stargrass.Tiles;
 
+[AutoloadGlowmask("Method:Content.Forest.Stargrass.Tiles.StargrassTile Glow")] //Use Stargrass' glow
 internal class StargrassVine : ModTile
 {
-	private static Asset<Texture2D> _glow = null;
-
 	public override void SetStaticDefaults()
 	{
 		Main.tileSolid[Type] = false;
@@ -20,40 +19,23 @@ internal class StargrassVine : ModTile
 
 		DustType = DustID.Grass;
 		HitSound = SoundID.Grass;
-
-		_glow = ModContent.Request<Texture2D>(Texture + "_Glow");
 	}
 
 	public override void NumDust(int i, int j, bool fail, ref int num) => num = 3;
 
-	public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
-	{
-		if (Main.tile[i, j + 1].TileType == Type)
-			WorldGen.KillTile(i, j + 1, false, false, true);
-	}
-
-	public override void NearbyEffects(int i, int j, bool closer)
+	public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
 	{
 		if (!Main.tile[i, j - 1].HasTile)
 			WorldGen.KillTile(i, j);
+
+		return true;
 	}
 
 	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
 	{
-		Tile t = Framing.GetTileSafely(i, j);
-		float sine = (float)Math.Sin((i + j) * MathHelper.ToRadians(20) + Main.GameUpdateCount * 0.02f) * 1f;
+		if (Main.LightingEveryFrame)
+			Main.instance.TilesRenderer.CrawlToTopOfVineAndAddSpecialPoint(j, i);
 
-		if (Main.tile[i, j - 1].TileType != Type)
-			sine = 0;
-		else if (Main.tile[i, j - 2].TileType != Type)
-			sine *= 0.33f;
-		else if (Main.tile[i, j - 3].TileType != Type)
-			sine *= 0.67f;
-
-		var source = new Rectangle(t.TileFrameX, t.TileFrameY, 16, 16);
-		Vector2 position = this.DrawPosition(i, j, new Vector2(sine, 0));
-		spriteBatch.Draw(TextureAssets.Tile[Type].Value, position, source, Lighting.GetColor(i, j), 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-		spriteBatch.Draw(_glow.Value, position, source, StargrassTile.Glow(new Point(i, j)), 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
 		return false;
 	}
 }
