0745abd180636735e0ce65e7291f60cd61cb1f4b
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-01-01 21:54:06 -0400

    StaticWorldSeed multiplayer fix

--- SpiritReforgedSystem.cs
+++ SpiritReforgedSystem.cs
@@ -1,23 +1,27 @@
 ﻿using SpiritReforged.Common.Particle;
+using System.IO;
 
 namespace SpiritReforged;
 
 public class SpiritReforgedSystem : ModSystem
 {
-	private static int staticWorldSeed = -1;
+	private static int StaticWorldSeed = -1;
 
 	internal static int GetWorldSeed()
 	{
-		if (staticWorldSeed > -1)
-			return staticWorldSeed;
+		if (StaticWorldSeed > -1)
+			return StaticWorldSeed;
 
-		if (!int.TryParse(WorldGen.currentWorldSeed, out int seed))
+		int seed = -1;
+		if (WorldGen.currentWorldSeed is not null && !int.TryParse(WorldGen.currentWorldSeed, out seed))
 			seed = ReLogic.Utilities.Crc32.Calculate(WorldGen.currentWorldSeed);
 
-		return staticWorldSeed = (seed == int.MinValue) ? int.MaxValue : Math.Abs(seed); //Parse the world seed and store it
+		return StaticWorldSeed = (seed == int.MinValue) ? int.MaxValue : Math.Abs(seed); //Parse the world seed and store it
 	}
 
-	public override void OnWorldUnload() => staticWorldSeed = -1;
+	public override void OnWorldUnload() => StaticWorldSeed = -1;
+	public override void NetSend(BinaryWriter writer) => writer.Write(StaticWorldSeed);
+	public override void NetReceive(BinaryReader reader) => StaticWorldSeed = reader.ReadInt32();
 
 	public override void PreUpdateItems()
 	{
