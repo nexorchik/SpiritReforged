b5b203473ed96678b1961e1e5ddb6566f85bddaf
Abhay M <amanchala13@gmail.com>
2025-01-07 17:37:02 +0100

    Cleanup

--- Content/Forest/ArcaneNecklace/ArcaneNecklaceGNPC.cs
+++ Content/Forest/ArcaneNecklace/ArcaneNecklaceGNPC.cs
@@ -17,15 +17,16 @@ namespace SpiritReforged.Content.Forest.ArcaneNecklace
 				for (int i = 0; i < 4; i++)
 				{
 					ParticleHandler.SpawnParticle(new GlowParticle(target.Center, Main.rand.NextVector2CircularEdge(1, 1) * Main.rand.NextFloat(1f, 2f), new Color(74, 110, 255), Main.rand.NextFloat(0.3f, 0.5f), Main.rand.Next(30, 50), 12, delegate (Particle p) { p.Velocity.Y *= -1f; }));
-					ParticleHandler.SpawnParticle(new GlowParticle(target.Center, Main.rand.NextVector2CircularEdge(1, 1) * Main.rand.NextFloat(1f, 2f), new Color(74, 110, 255), Main.rand.NextFloat(0.1f, 0.3f), Main.rand.Next(30, 50), 12, delegate (Particle p) { p.Velocity.Y -= .05f; }));
+					ParticleHandler.SpawnParticle(new GlowParticle(target.Center, Main.rand.NextVector2CircularEdge(1, 1) * Main.rand.NextFloat(1f, 2f), new Color(74, 110, 255), Main.rand.NextFloat(0.1f, 0.3f), Main.rand.Next(30, 50), 12, delegate (Particle p) { p.Velocity.Y -= .025f; }));
 				}
+
 				target.DropItemInstanced(target.position, target.Size, ItemID.Star);
 			}
 		}
 
 		public override void ModifyHitByItem(NPC npc, Player player, Item item, ref NPC.HitModifiers modifiers)
 		{
-			if ((player.HasAccessory<ArcaneNecklaceGold>() || player.HasAccessory<ArcaneNecklacePlatinum>()) && player.HeldItem.DamageType == DamageClass.Magic && player.statMana < player.statManaMax2)
+			if ((player.HasAccessory<ArcaneNecklaceGold>() || player.HasAccessory<ArcaneNecklacePlatinum>()) && item.DamageType == DamageClass.Magic && player.statMana < player.statManaMax2)
 				DropStar(npc);
 		}
 
