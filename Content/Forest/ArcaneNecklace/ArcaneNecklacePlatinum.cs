b2503cdb5354041a241a27f5b7594822cef50dc8
Abhay M <amanchala13@gmail.com>
2025-01-07 17:53:15 +0100

    Made Requested changes + More misc cleanup

--- Content/Forest/ArcaneNecklace/ArcaneNecklacePlatinum.cs
+++ Content/Forest/ArcaneNecklace/ArcaneNecklacePlatinum.cs
@@ -1,12 +1,7 @@
-using SpiritReforged.Common.ItemCommon;
-
 namespace SpiritReforged.Content.Forest.ArcaneNecklace;
 
 [AutoloadEquip(EquipType.Neck)]
 public class ArcaneNecklacePlatinum : ArcaneNecklaceGold
 {
-	public override bool CanAccessoryBeEquippedWith(Item equippedItem, Item incomingItem, Player player)
-	{
-		return incomingItem.type != ModContent.ItemType<ArcaneNecklaceGold>();
-	}
+	public override bool CanAccessoryBeEquippedWith(Item equippedItem, Item incomingItem, Player player) => incomingItem.type != ModContent.ItemType<ArcaneNecklaceGold>();
 }
