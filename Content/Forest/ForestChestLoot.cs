fd2b170fd4e0fe9353a37c4c30852c57dd660103
Abhay M <amanchala13@gmail.com>
2025-01-08 10:01:19 +0530

    Add Jump Potion to Forest Chest Loot

--- Content/Forest/ForestChestLoot.cs
+++ Content/Forest/ForestChestLoot.cs
@@ -1,5 +1,6 @@
 ﻿using SpiritReforged.Common.Misc;
 using SpiritReforged.Content.Forest.ArcaneNecklace;
+using SpiritReforged.Content.Forest.Cloudstalk.Items;
 using SpiritReforged.Content.Forest.RoguesCrest;
 
 namespace SpiritReforged.Content.Underground;
@@ -10,5 +11,6 @@ public class ForestChestLoot : ModSystem
 	{
 		ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<RogueCrest>() }, 1, 0.25f), ChestPoolUtils.woodChests, 1);
 		ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<ArcaneNecklaceGold>(), ModContent.ItemType<ArcaneNecklacePlatinum>() }, 1, 0.125f), ChestPoolUtils.woodChests, 1);
+		ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<DoubleJumpPotion>() }, Main.rand.Next(1, 3), 0.35f), ChestPoolUtils.woodChests, 4);
 	}
 }
