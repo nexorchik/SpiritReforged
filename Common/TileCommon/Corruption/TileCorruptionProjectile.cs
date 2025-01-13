d01f535c6e9dbced3f9d763a5242ca8d8452bda0
GabeHasWon <gabriel@guevarameyer.com>
2025-01-12 22:02:02 -0600

    waters conversions

--- Common/TileCommon/Corruption/TileCorruptionProjectile.cs
+++ Common/TileCommon/Corruption/TileCorruptionProjectile.cs
@@ -1,12 +1,18 @@
-﻿namespace SpiritReforged.Common.TileCommon.Corruption;
+﻿using Terraria.DataStructures;
+
+namespace SpiritReforged.Common.TileCommon.Corruption;
 
 internal class TileCorruptionProjectile : GlobalProjectile
 {
 	public override bool AppliesToEntity(Projectile entity, bool lateInstantiation) => entity.type is ProjectileID.PurificationPowder or ProjectileID.ViciousPowder 
-		or ProjectileID.VilePowder or ProjectileID.CorruptSpray or ProjectileID.CrimsonSpray or ProjectileID.HallowSpray or ProjectileID.PureSpray;
+		or ProjectileID.VilePowder or ProjectileID.CorruptSpray or ProjectileID.CrimsonSpray or ProjectileID.HallowSpray or ProjectileID.PureSpray or ProjectileID.HolyWater
+		or ProjectileID.UnholyWater or ProjectileID.BloodWater;
 
 	public override void AI(Projectile projectile)
 	{
+		if (projectile.type is ProjectileID.HolyWater or ProjectileID.UnholyWater or ProjectileID.BloodWater)
+			return;
+
 		ConversionType type = projectile.type switch
 		{
 			ProjectileID.VilePowder or ProjectileID.CorruptSpray => ConversionType.Corrupt,
@@ -35,5 +41,26 @@ internal class TileCorruptionProjectile : GlobalProjectile
 				TileCorruptor.Convert(projectile.GetSource_FromThis(), type, i, j);
 			}
 		}
+	}
+
+	public override void OnKill(Projectile projectile, int timeLeft)
+	{
+		if (projectile.type is not (ProjectileID.HolyWater or ProjectileID.UnholyWater or ProjectileID.BloodWater))
+			return;
+
+		ConversionType type = projectile.type switch
+		{
+			ProjectileID.UnholyWater => ConversionType.Corrupt,
+			ProjectileID.BloodWater => ConversionType.Crimson,
+			ProjectileID.HolyWater => ConversionType.Hallow,
+			_ => ConversionType.Purify
+		};
+
+		Point16 pos = projectile.Center.ToTileCoordinates16();
+
+		for (int i = pos.X - 20; i < pos.X + 20; ++i)
+			for (int j = pos.Y - 20; j < pos.Y + 20; ++j)
+				if (Vector2.Distance(new Vector2(i, j), pos.ToVector2()) < 5 && WorldGen.InWorld(i, j, WorldGen.InfectionAndGrassSpreadOuterWorldBuffer))
+					TileCorruptor.Convert(projectile.GetSource_Death(), type, i, j);
 	}
 }
