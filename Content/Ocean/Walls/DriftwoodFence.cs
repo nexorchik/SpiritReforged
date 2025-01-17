5f56da5198a4c073f0973caf270e9dd3d53c21d7
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-01-17 15:11:46 -0400

    Fence recipe fix

--- Content/Ocean/Walls/DriftwoodFence.cs
+++ Content/Ocean/Walls/DriftwoodFence.cs
@@ -7,15 +7,13 @@ public class DriftwoodFence : ModWall, IAutoloadWallItem
 {
 	public void AddItemRecipes(ModItem item)
 	{
-		var mod = SpiritReforgedMod.Instance; //Mod is null here, so get the instance manually
-
 		item.CreateRecipe(4)
 			.AddIngredient(ModContent.ItemType<DriftwoodTileItem>())
 			.AddTile(TileID.WorkBenches)
 			.Register();
 
 		//Allow fence items to be crafted back into base materials
-		Recipe.Create(mod.Find<ModItem>("DriftwoodFenceItem").Type)
+		Recipe.Create(ModContent.ItemType<DriftwoodTileItem>())
 			.AddIngredient(item.Type, 4)
 			.AddTile(TileID.WorkBenches)
 			.Register();
