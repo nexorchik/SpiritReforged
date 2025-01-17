474d853c72ef307d96390692763e471d26461f21
GabeHasWon <gabriel@guevarameyer.com>
2025-01-17 17:16:46 -0600

    add new scrolling mod icon

---
+++ Common/UI/Misc/UIScrollingImage.cs
@@ -0,0 +1,30 @@
+﻿using Terraria.UI;
+
+namespace SpiritReforged.Common.UI.Misc;
+
+public class UIScrollingImage : UIElement
+{
+	private readonly Asset<Texture2D> Border;
+	private readonly Asset<Texture2D> Scrolling;
+	private readonly float ScrollSpeed;
+
+	private float _timer = 0;
+
+	public UIScrollingImage(Asset<Texture2D> border, Asset<Texture2D> scrolling, float scrollSpeed)
+	{
+		Border = border;
+		Scrolling = scrolling;
+		ScrollSpeed = scrollSpeed;
+
+		OverrideSamplerState = SamplerState.PointWrap;
+	}
+
+	protected override void DrawSelf(SpriteBatch spriteBatch)
+	{
+		_timer += ScrollSpeed;
+
+		Vector2 pos = GetDimensions().Position();
+		spriteBatch.Draw(Scrolling.Value, pos, new Rectangle((int)_timer, 0, 80, 80), Color.White);
+		spriteBatch.Draw(Border.Value, pos, Color.White);
+	}
+}
