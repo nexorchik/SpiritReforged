a85a0b0ad17329dfa583169bbf99a1e87fe53fab
GabeHasWon <gabriel@guevarameyer.com>
2025-01-18 13:20:53 -0600

    dye crate in its entirety

--- Content/Jungle/JungleGeneration.cs
+++ Content/Jungle/JungleGeneration.cs
@@ -1,10 +1,16 @@
 ﻿using SpiritReforged.Common.Misc;
 using SpiritReforged.Common.WorldGeneration;
+using SpiritReforged.Content.Jungle.Misc.DyeCrate;
 using SpiritReforged.Content.Jungle.Toucane;
 
 namespace SpiritReforged.Content.Jungle;
 
 public class JungleGeneration : ModSystem
 {
-	public override void PostWorldGen() => ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<ToucaneItem>() }, 1, 0.25f), (int)VanillaChestID.Ivy, 1);
+	public override void PostWorldGen()
+	{
+		ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<ToucaneItem>() }, 1, 0.25f), (int)VanillaChestID.Ivy, 1);
+		ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<DyeCrateItem>() }, 1, 0.5f), (int)VanillaChestID.Ivy, 1);
+		ChestPoolUtils.AddToVanillaChest(new ChestPoolUtils.ChestInfo(new int[] { ModContent.ItemType<DyeCrateItem>() }, 1, 0.33f), (int)VanillaChestID.Jungle, 1);
+	}
 }
