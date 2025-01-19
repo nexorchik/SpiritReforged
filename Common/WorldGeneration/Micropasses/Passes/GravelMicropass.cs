fae72a46419420ffdcd8a7b78f5dcf8856355952
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-01-19 12:59:14 -0400

    Increase Hydrothermal Vent gen amount + growing

--- Common/WorldGeneration/Micropasses/Passes/GravelMicropass.cs
+++ Common/WorldGeneration/Micropasses/Passes/GravelMicropass.cs
@@ -23,7 +23,7 @@ internal class GravelMicropass : Micropass
 		progress.Message = Language.GetTextValue("Mods.SpiritReforged.Generation.Gravel");
 
 		int count = 0, fails = 0;
-		int maxCount = 6 + 2 * WorldGen.GetWorldSize(); //The maximum number of gravel patches which can generate in a world
+		int maxCount = 8 + 4 * WorldGen.GetWorldSize(); //The maximum number of gravel patches which can generate in a world
 		int distance = WorldGen.oceanDistance;
 
 		int waterTopLeft = 0, waterTopRight = 0;
