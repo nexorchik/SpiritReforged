31654f91378b917ca7e9c1480eaf5d52a4606b31
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-01-20 00:14:27 -0400

    Change Beach Towel equip type

--- Content/Ocean/Items/Vanity/BeachTowel.cs
+++ Content/Ocean/Items/Vanity/BeachTowel.cs
@@ -1,14 +1,14 @@
 namespace SpiritReforged.Content.Ocean.Items.Vanity;
 
-[AutoloadEquip(EquipType.Body)]
+[AutoloadEquip(EquipType.HandsOn)]
 public class BeachTowel : ModItem
 {
 	public override void SetDefaults()
 	{
-		Item.width = 26;
-		Item.height = 26;
+		Item.width = Item.height = 26;
 		Item.value = Item.buyPrice(0, 5, 0, 0);
 		Item.rare = ItemRarityID.Blue;
+		Item.accessory = true;
 		Item.vanity = true;
 	}
 }
