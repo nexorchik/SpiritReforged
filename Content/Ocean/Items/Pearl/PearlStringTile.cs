fa2185623cc49d7b75a022128214f607b84513c9
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-01-23 11:47:15 -0400

    Pearl String tile right-click & rubble

--- Content/Ocean/Items/Pearl/PearlStringTile.cs
+++ Content/Ocean/Items/Pearl/PearlStringTile.cs
@@ -1,14 +1,13 @@
 using SpiritReforged.Common.Particle;
 using SpiritReforged.Common.TileCommon;
+using SpiritReforged.Common.Visuals.Glowmasks;
 using Terraria.DataStructures;
 
 namespace SpiritReforged.Content.Ocean.Items.Pearl;
 
-[DrawOrder(DrawOrderAttribute.Layer.Solid)] //Draw over sand
+[AutoloadGlowmask("255,255,255", false)]
 public class PearlStringTile : ModTile
 {
-	private static Asset<Texture2D> glowTexture;
-
 	public override void SetStaticDefaults()
 	{
 		Main.tileSolid[Type] = false;
@@ -16,7 +15,7 @@ public class PearlStringTile : ModTile
 		Main.tileNoFail[Type] = true;
 
 		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x1);
-		TileObjectData.newTile.CoordinateHeights = [18];
+		TileObjectData.newTile.CoordinateHeights = [16];
 		TileObjectData.newTile.Origin = new(1, 0);
 		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile, 2, 0);
 		TileObjectData.newTile.AnchorValidTiles = [TileID.Sand, TileID.Ebonsand, TileID.Crimsand, TileID.Pearlsand];
@@ -24,10 +23,43 @@ public class PearlStringTile : ModTile
 
 		AddMapEntry(new Color(100, 100, 120));
 		RegisterItemDrop(ModContent.ItemType<PearlString>());
+		SolidBottomGlobalTile.solidBottomTypes.Add(Type);
 
-		DustType = -1; //No dust
+		DustType = DustID.Sand;
+	}
 
-		glowTexture = ModContent.Request<Texture2D>(Texture + "_Glow");
+	public override void MouseOver(int i, int j)
+	{
+		Player player = Main.LocalPlayer;
+		player.noThrow = 2;
+		player.cursorItemIconEnabled = true;
+		player.cursorItemIconID = ModContent.ItemType<PearlString>();
+	}
+
+	public override bool RightClick(int i, int j)
+	{
+		int y = 0;
+		TileExtensions.GetTopLeft(ref i, ref y);
+
+		WorldGen.KillTile(i, j);
+		if (Main.netMode != NetmodeID.SinglePlayer)
+			NetMessage.SendTileSquare(-1, i, j, 2, 1);
+
+		return true;
+	}
+
+	public override bool CreateDust(int i, int j, ref int type)
+	{
+		var tile = Framing.GetTileSafely(i, j);
+		type = (tile.TileFrameY / 18) switch
+		{
+			1 => DustID.Corruption,
+			2 => DustID.Crimson,
+			3 => DustID.Pearlsand,
+			_ => DustID.Sand,
+		};
+
+		return true;
 	}
 
 	public override void PostTileFrame(int i, int j, int up, int down, int left, int right, int upLeft, int upRight, int downLeft, int downRight)
@@ -60,12 +92,13 @@ public class PearlStringTile : ModTile
 		var texture = TextureAssets.Tile[tile.TileType].Value;
 
 		var source = new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, 16);
-		var position = new Vector2(i, j) * 16 - Main.screenPosition + new Vector2(0, 14);
+		var offset = Lighting.LegacyEngine.Mode > 1 && Main.GameZoomTarget == 1 ? Vector2.Zero : Vector2.One * 12;
+		var position = (new Vector2(i, j) + offset) * 16 - Main.screenPosition;
 
 		spriteBatch.Draw(texture, position, source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, SpriteEffects.None, 0);
-		spriteBatch.Draw(glowTexture.Value, position, source, Lighting.GetColor(i, j) * 2, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+		spriteBatch.Draw(GlowmaskTile.TileIdToGlowmask[Type].Glowmask.Value, position, source, Lighting.GetColor(i, j) * 2, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
 
-		var rect = new Rectangle(i * 16, j * 16 + 14, 16, 16);
+		var rect = new Rectangle(i * 16, j * 16, 16, 16);
 		if (!Main.gamePaused && Main.rand.NextBool(50) && Main.LocalPlayer.Distance(rect.Center()) < 100) //Nearby dust effects
 		{
 			var dustPos = Main.rand.NextVector2FromRectangle(rect);
@@ -80,4 +113,29 @@ public class PearlStringTile : ModTile
 
 		return false;
 	}
+}
+
+public class PearlStringTileRubble : PearlStringTile
+{
+	public override string Texture => base.Texture.Remove(base.Texture.Length - 6, 6); //Remove "Rubble"
+
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+		FlexibleTileWand.RubblePlacementSmall.AddVariation(ModContent.ItemType<PearlString>(), Type, 0);
+	}
+
+	public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+	{
+		if (Main.netMode == NetmodeID.MultiplayerClient)
+			return;
+
+		int item = Item.NewItem(null, new Rectangle(i * 16, j * 16, 16, 16), ModContent.ItemType<PearlString>());
+		if (Main.netMode != NetmodeID.SinglePlayer)
+			NetMessage.SendData(MessageID.SyncItem, number: item);
+	}
+
+	public override bool CanDrop(int i, int j) => false; //Don't drop the default item
+	public override void MouseOver(int i, int j) { }
+	public override bool RightClick(int i, int j) => false;
 }
