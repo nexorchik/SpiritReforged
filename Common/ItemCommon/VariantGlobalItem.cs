f51391f617f7f8b456d0e1de7c6a07ae672b16de
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-01-28 18:03:53 -0400

    Variant item system

---
+++ Common/ItemCommon/VariantGlobalItem.cs
@@ -0,0 +1,140 @@
+﻿using System.IO;
+using Terraria.DataStructures;
+using Terraria.ModLoader.IO;
+using Terraria.UI;
+
+namespace SpiritReforged.Common.ItemCommon;
+
+internal class VariantGlobalItem : GlobalItem
+{
+	public override bool InstancePerEntity => true;
+
+	private struct VarData(Point[] sizes, bool inventory)
+	{
+		public Point[] sizes = sizes;
+		public bool inventory = inventory;
+	}
+
+	private static readonly Dictionary<int, VarData> variantData = []; //Type, frame sizes
+	private int subID = -1;
+
+	/// <summary> Registers the given item <paramref name="type"/> as having variants of <paramref name="amount"/>.<para/>
+	/// The item texture must be a vertical sheet with 2px padding to work correctly. </summary>
+	/// <param name="type"> The item type. </param>
+	/// <param name="amount"> The number of variants. </param>
+	/// <param name="appliesToInventory"> Whether this item can use variants in the inventory. </param>
+	public static void AddVariants(int type, int amount, bool appliesToInventory = false)
+	{
+		variantData.Add(type, new VarData(null, appliesToInventory));
+		Main.RegisterItemAnimation(type, new DrawAnimationVertical(2, amount) { NotActuallyAnimating = true });
+	}
+
+	/// <summary><inheritdoc cref="AddVariants(int, int, bool)"/></summary>
+	/// <param name="type"> The item type. </param>
+	/// <param name="sizes"> The dimensions of each individual frame. Also determines the frame count based on length. </param>
+	/// <param name="appliesToInventory"> Whether this item can use variants in the inventory. </param>
+	public static void AddVariants(int type, Point[] sizes, bool appliesToInventory = false)
+	{
+		variantData.Add(type, new VarData(sizes, appliesToInventory));
+		Main.RegisterItemAnimation(type, new DrawAnimationVertical(2, sizes.Length) { NotActuallyAnimating = true });
+	}
+
+	/// <returns> Which variant this item is using. </returns>
+	public static int GetVariant(Item item) => item.TryGetGlobalItem(out VariantGlobalItem vItem) ? vItem.subID : 0;
+
+	/// <summary> Gets the draw frame of this varianted <paramref name="item"/>. </summary>
+	/// <param name="item"> The item. </param>
+	/// <returns> <see cref="Rectangle.Empty"/> if the item is invalid. </returns>
+	public static Rectangle GetSource(Item item)
+	{
+		if (item.TryGetGlobalItem(out VariantGlobalItem vItem))
+			return vItem.GetSource(item.type);
+
+		return Rectangle.Empty;
+	}
+
+	private Rectangle GetSource(int type)
+	{
+		int frameCount = Main.itemAnimations[type].FrameCount;
+		int frame = Math.Max(subID, 0);
+
+		var texture = TextureAssets.Item[type].Value;
+		var rectangle = texture.Frame(1, frameCount, 0, frame, 0, -2);
+		var sizes = variantData[type].sizes;
+
+		if (sizes != null && frame < sizes.Length)
+		{
+			rectangle.Width = sizes[frame].X;
+			rectangle.Height = sizes[frame].Y;
+		}
+
+		return rectangle;
+	}
+
+	public override bool AppliesToEntity(Item entity, bool lateInstantiation) => variantData.ContainsKey(entity.type);
+
+	public override void SetDefaults(Item item)
+	{
+		if (variantData[item.type].inventory && Main.netMode != NetmodeID.MultiplayerClient)
+		{
+			int frameCount = Main.itemAnimations[item.type].FrameCount;
+			subID = Main.rand.Next(frameCount);
+
+			if (Main.netMode != NetmodeID.SinglePlayer)
+				NetMessage.SendData(MessageID.SyncItem, number: item.whoAmI);
+		}
+	}
+
+	public override bool PreDrawInWorld(Item item, SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+	{
+		int frameCount = Main.itemAnimations[item.type].FrameCount;
+
+		if (!variantData[item.type].inventory && subID == -1)
+			subID = Main.rand.Next(frameCount);
+
+		var texture = TextureAssets.Item[item.type].Value;
+		var source = GetSource(item.type);
+
+		spriteBatch.Draw(texture, item.position + item.Size - source.Size() - Main.screenPosition, source, GetAlpha(item, lightColor) ?? lightColor, rotation, Vector2.Zero, scale, SpriteEffects.None, 0);
+		return false;
+	}
+
+	public override bool PreDrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+	{
+		if (!variantData[item.type].inventory)
+			subID = -1;
+
+		var texture = TextureAssets.Item[item.type].Value;
+		var source = GetSource(item.type);
+
+		//Scale the item according to 'source' instead of 'frame'
+		ItemSlot.DrawItem_GetColorAndScale(item, Main.inventoryScale, ref drawColor, 32, ref source, out _, out scale);
+
+		spriteBatch.Draw(texture, position, source, drawColor, 0, source.Size() / 2, scale, SpriteEffects.None, 0);
+		return false;
+	}
+
+	public override void SaveData(Item item, TagCompound tag)
+	{
+		if (variantData[item.type].inventory)
+			tag[nameof(subID)] = (byte)subID;
+	}
+
+	public override void LoadData(Item item, TagCompound tag)
+	{
+		if (variantData[item.type].inventory)
+			subID = tag.Get<byte>(nameof(subID));
+	}
+
+	public override void NetSend(Item item, BinaryWriter writer)
+	{
+		if (variantData[item.type].inventory)
+			writer.Write((byte)subID);
+	}
+
+	public override void NetReceive(Item item, BinaryReader reader)
+	{
+		if (variantData[item.type].inventory)
+			subID = reader.ReadByte();
+	}
+}
--- Content/Ocean/Items/Driftwood/FloatingDriftwood.cs
+++ Content/Ocean/Items/Driftwood/FloatingDriftwood.cs
@@ -1,3 +1,4 @@
+using SpiritReforged.Common.ItemCommon;
 using SpiritReforged.Common.ItemCommon.FloatingItem;
 
 namespace SpiritReforged.Content.Ocean.Items.Driftwood;
@@ -5,15 +6,11 @@ namespace SpiritReforged.Content.Ocean.Items.Driftwood;
 //The naturally spawning variant of our driftwood decoration items and the player's source of driftwood blocks
 public class FloatingDriftwood : FloatingItem
 {
-	private int subID = -1; //Controls the in-world sprite for this item
-
 	public override float Weight => base.Weight * 0.9f;
 	public override float Bouyancy => base.Bouyancy * 1.05f;
-
 	public override string Texture => base.Texture.Replace("Floating", string.Empty);
 
-	public override void SetStaticDefaults() => Main.RegisterItemAnimation(Type, new Terraria.DataStructures.DrawAnimationVertical(2, 3) { NotActuallyAnimating = true });
-
+	public override void SetStaticDefaults() => VariantGlobalItem.AddVariants(Type, 3);
 	public override void SetDefaults()
 	{
 		Item.width = 30;
@@ -24,7 +21,7 @@ public class FloatingDriftwood : FloatingItem
 
 	public override bool OnPickup(Player player)
 	{
-		int stack = subID switch
+		int stack = VariantGlobalItem.GetVariant(Item) switch
 		{
 			1 => 20,
 			2 => 25,
@@ -33,21 +30,5 @@ public class FloatingDriftwood : FloatingItem
 
 		player.QuickSpawnItem(player.GetSource_OpenItem(Item.type, "Pickup"), ModContent.ItemType<DriftwoodTileItem>(), stack);
 		return false;
-	}
-
-	public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
-	{
-		if (subID == -1)
-			subID = Main.rand.Next(3);
-
-		Texture2D tex = TextureAssets.Item[Type].Value;
-		spriteBatch.Draw(tex, Item.position - Main.screenPosition, new Rectangle(0, 22 * subID, 36, 22), GetAlpha(lightColor) ?? lightColor, rotation, Vector2.Zero, scale, SpriteEffects.None, 0f);
-		return false;
-	}
-
-	public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
-	{
-		subID = -1;
-		return true;
 	}
 }
