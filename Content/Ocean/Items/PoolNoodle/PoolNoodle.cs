d08fb5612432a534e75088b6a572087bcd1cd16e
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-02-05 08:33:18 -0400

    Pool Noodle fix + VariantGlobalItem changes

--- Content/Ocean/Items/PoolNoodle/PoolNoodle.cs
+++ Content/Ocean/Items/PoolNoodle/PoolNoodle.cs
@@ -1,25 +1,59 @@
 using SpiritReforged.Common.ItemCommon;
+using System.IO;
 using Terraria.DataStructures;
+using Terraria.ModLoader.IO;
 
 namespace SpiritReforged.Content.Ocean.Items.PoolNoodle;
 
 public class PoolNoodle : ModItem
 {
-	public override void SetStaticDefaults() => VariantGlobalItem.AddVariants(Type, 3, true);
+	protected override bool CloneNewInstances => true;
+
+	public const int NumStyles = 3;
+	public byte style = NumStyles;
+
+	public override void SetStaticDefaults() => VariantGlobalItem.AddVariants(Type, NumStyles, false);
 	public override void SetDefaults()
 	{
 		Item.DefaultToWhip(ModContent.ProjectileType<PoolNoodleProj>(), 14, 0, 4);
 		Item.width = Item.height = 38;
 		Item.rare = ItemRarityID.Blue;
 		Item.value = Item.sellPrice(silver: 30);
+
+		style = (byte)Main.rand.Next(NumStyles);
+	}
+
+	public override ModItem Clone(Item itemClone)
+	{
+		var myClone = (PoolNoodle)base.Clone(itemClone);
+		myClone.style = style;
+		return myClone;
 	}
 
 	public override bool MeleePrefix() => true;
 	public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 	{
-		int variant = VariantGlobalItem.GetVariant(Item);
-		Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, ai1: variant);
-
+		Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI, ai1: style);
 		return false;
+	}
+
+	public override void SaveData(TagCompound tag) => tag[nameof(style)] = style;
+	public override void LoadData(TagCompound tag)
+	{
+		style = tag.Get<byte>(nameof(style));
+		SetVisualStyle();
+	}
+
+	public override void NetSend(BinaryWriter writer) => writer.Write(style);
+	public override void NetReceive(BinaryReader reader)
+	{
+		style = reader.ReadByte();
+		SetVisualStyle();
+	}
+
+	private void SetVisualStyle()
+	{
+		if (!Main.dedServ && Item.TryGetGlobalItem(out VariantGlobalItem v))
+			v.subID = style;
 	}
 }
