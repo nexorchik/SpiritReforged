e6b87da0bd4f3f1a9c7a940b4b14ce8f2ca90341
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-02-09 12:32:16 -0400

    Dust storm evils

--- Content/Savanna/DustStorm/DustStormPlayer.cs
+++ Content/Savanna/DustStorm/DustStormPlayer.cs
@@ -1,7 +1,17 @@
-﻿namespace SpiritReforged.Content.Savanna.DustStorm;
+﻿using SpiritReforged.Content.Savanna.Biome;
+
+namespace SpiritReforged.Content.Savanna.DustStorm;
 
 public class DustStormPlayer : ModPlayer
 {
 	/// <summary> Whether the player is present in a dust storm. </summary>
-	public bool ZoneDustStorm => (Math.Abs(Main.windSpeedCurrent) > .4f || Player.ZoneSandstorm) && Player.InModBiome<Biome.SavannaBiome>();
+	public bool ZoneDustStorm => (Math.Abs(Main.windSpeedCurrent) > .4f || Player.ZoneSandstorm) && (Player.InModBiome<SavannaBiome>() || EvilSavanna());
+
+	private bool EvilSavanna()
+	{
+		if (SavannaTileCounts.InSavanna)
+			return Player.ZoneCorrupt || Player.ZoneCrimson || Player.ZoneHallow;
+
+		return false;
+	}
 }
