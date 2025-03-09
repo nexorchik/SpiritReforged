99101a33b737dd97494d866bec5ab3ab658fda8a
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-03-09 17:43:45 -0300

    Rescale Koi Totem

--- Content/Ocean/Items/KoiTotem/KoiTotem.cs
+++ Content/Ocean/Items/KoiTotem/KoiTotem.cs
@@ -1,7 +1,9 @@
 using MonoMod.Cil;
 using SpiritReforged.Common.ItemCommon.FloatingItem;
 using SpiritReforged.Common.Particle;
+using SpiritReforged.Common.TileCommon.DrawPreviewHook;
 using SpiritReforged.Content.Particles;
+using Terraria.DataStructures;
 
 namespace SpiritReforged.Content.Ocean.Items.KoiTotem;
 
@@ -65,7 +67,7 @@ public class KoiTotem : FloatingItem
 	}
 }
 
-public class KoiTotemTile : ModTile
+public class KoiTotemTile : ModTile, IDrawPreview
 {
 	public override void SetStaticDefaults()
 	{
@@ -73,10 +75,11 @@ public class KoiTotemTile : ModTile
 		Main.tileNoAttach[Type] = true;
 		Main.tileLavaDeath[Type] = true;
 
-		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+		TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
 		TileObjectData.newTile.Height = 4;
-		TileObjectData.newTile.Origin = new(1, 3);
+		TileObjectData.newTile.Origin = new(0, 3);
 		TileObjectData.newTile.CoordinateHeights = [16, 16, 16, 18];
+		TileObjectData.newTile.CoordinateWidth = 18;
 		TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
 		TileObjectData.newTile.StyleWrapLimit = 2; 
 		TileObjectData.newTile.StyleMultiplier = 2; 
@@ -118,5 +121,27 @@ public class KoiTotemTile : ModTile
 					ParticleHandler.SpawnParticle(new DissipatingImage(pos + new Vector2(24, 0), Color.Cyan * .15f, 0, .25f, 1f, "Bloom", 120));
 			}
 		}
+	}
+
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		var tile = Main.tile[i, j];
+		var texture = TextureAssets.Tile[tile.TileType].Value;
+
+		var frame = new Point(tile.TileFrameX, tile.TileFrameY);
+		var source = new Rectangle(frame.X, frame.Y, 18, 18);
+
+		var zero = Main.drawToScreen ? Vector2.Zero : new Vector2(Main.offScreenRange);
+		int offX = (frame.X % TileObjectData.GetTileData(Type, 0).CoordinateFullWidth == 0) ? -2 : 0;
+		var position = new Vector2(i, j) * 16 - Main.screenPosition + zero + new Vector2(offX, 0);
+
+		spriteBatch.Draw(texture, position, source, Lighting.GetColor(i, j), 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+		return false;
+	}
+
+	public void DrawPreview(SpriteBatch spriteBatch, TileObjectPreviewData data, Vector2 position)
+	{
+
 	}
 }
