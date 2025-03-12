93aad84474f1b7042a4c0f7b3ab125a353365e29
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-03-12 20:24:59 -0300

    Butterfly Shrine walls

--- Common/WorldGeneration/Micropasses/Passes/ButterflyMicropass.cs
+++ Common/WorldGeneration/Micropasses/Passes/ButterflyMicropass.cs
@@ -56,6 +56,9 @@ internal class ButterflyMicropass : Micropass
 					var data = new ShapeData();
 					ushort[] ignore = [TileID.LivingWood, TileID.LeafBlock, TileID.BlueDungeonBrick, TileID.GreenDungeonBrick, TileID.PinkDungeonBrick];
 
+					//Fill sand-type walls
+					WorldUtils.Gen(new Point(origin.X - 1, top.Y + 10), new Shapes.Rectangle(3, origin.Y - top.Y - 9), Actions.Chain(new Modifiers.Blotches(2, 0.2), new Modifiers.OnlyTiles(TileID.Sand, TileID.HardenedSand, TileID.Sandstone), new Modifiers.OnlyWalls(WallID.None), new Actions.PlaceWall(WallID.HardenedSand)));
+
 					WorldUtils.Gen(new Point(origin.X, top.Y + 10), new Shapes.Rectangle(1, origin.Y - top.Y - 9), Actions.Chain(new Modifiers.Blotches(2, 0.2), new Modifiers.SkipTiles(ignore), new Actions.ClearTile().Output(data), new Modifiers.Expand(1), new Modifiers.OnlyTiles(53), new Actions.SetTile(397).Output(data)));
 					WorldUtils.Gen(new Point(origin.X, top.Y + 10), new ModShapes.All(data), new Actions.SetFrames(frameNeighbors: true));
 				}
