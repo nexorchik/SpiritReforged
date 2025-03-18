6aa81610b204600611d02fc76f30370fde49b834
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-03-18 10:59:03 -0300

    ModifySmartTarget

---
+++ Common/TileCommon/ModifySmartTarget.cs
@@ -0,0 +1,50 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace SpiritReforged.Common.TileCommon;
+
+/// <summary> Can be applied to ModTiles. </summary>
+public interface IModifySmartTarget
+{
+	/// <summary> Modifies the final coordinates of smart cursor when in use. </summary>
+	public void ModifyTarget(ref int x, ref int y);
+}
+
+internal class ModifySmartTarget : ModSystem
+{
+	private static readonly HashSet<IModifySmartTarget> Loaded = [];
+
+	public override void Load() => IL_SmartCursorHelper.SmartCursorLookup += PostCoords;
+
+	public override void SetStaticDefaults()
+	{
+		foreach (var tile in Mod.GetContent<ModTile>())
+		{
+			if (tile is IModifySmartTarget target)
+				Loaded.Add(target);
+		}
+	}
+
+	private static void PostCoords(ILContext il)
+	{
+		var c = new ILCursor(il);
+
+		if (c.TryGotoNext(MoveType.After, x => x.MatchCall<SmartCursorHelper>("Step_StaffOfRegrowth")))
+		{
+			c.Emit(OpCodes.Ldloca_S, (byte)8);
+			c.Emit(OpCodes.Ldloca_S, (byte)9);
+			c.EmitDelegate(Modify);
+		}
+		else
+			SpiritReforgedMod.Instance.Logger.Debug($"IL edit '{nameof(PostCoords)}' failed.");
+	}
+
+	private static void Modify(ref int x, ref int y)
+	{
+		if (x == -1 || y == -1)
+			return;
+
+		foreach (var inst in Loaded)
+			inst.ModifyTarget(ref x, ref y);
+	}
+}
