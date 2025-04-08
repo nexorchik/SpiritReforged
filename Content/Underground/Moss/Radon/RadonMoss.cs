dba0ca1a498e6126bb52659033bde710d637135e
Abhay M <amanchala13@gmail.com>
2025-04-08 13:26:55 -0700

    Continue Radon Moss
    - I haven't figured out how to make the paint scraper specifically break the moss but almost done

--- Content/Underground/Moss/Radon/RadonMoss.cs
+++ Content/Underground/Moss/Radon/RadonMoss.cs
@@ -33,7 +33,51 @@ public class RadonMoss : GrassTile
 
 	public override void RandomUpdate(int i, int j)
 	{
-		if (SpreadHelper.Spread(i, j, Type, 4, DirtType) && Main.netMode != NetmodeID.SinglePlayer)
+		if (SpreadHelper.Spread(i, j, Type, 1, DirtType) && Main.netMode != NetmodeID.SinglePlayer)
+			NetMessage.SendTileSquare(-1, i, j, 3, TileChangeType.None); //Try spread moss
+
+		GrowTiles(i, j);
+	}
+
+	protected virtual void GrowTiles(int i, int j)
+	{
+		Point16[] offset = [new Point16(0, -1), new Point16(-1, 0), new Point16(1, 0), new Point16(0, 1)];
+		var coords = new Point16(i, j) + offset[Main.rand.Next(4)];
+
+		var current = Framing.GetTileSafely(coords);
+		if (!current.HasTile)
+		{
+			Placer.PlaceTile<RadonPlants>(coords.X, coords.Y, Main.rand.Next(RadonPlants.StyleRange));
+			return;
+		}
+	}
+}
+
+[AutoloadGlowmask("255,255,255")]
+public class RadonMossGrayBrick : GrassTile
+{
+	protected override int[] DirtType => [TileID.Stone, TileID.GrayBrick];
+	protected virtual Color MapColor => new(252, 248, 3);
+
+	public override void SetStaticDefaults()
+	{
+		base.SetStaticDefaults();
+
+		RegisterItemDrop(ItemID.GrayBrick);
+		AddMapEntry(MapColor);
+		this.Merge(TileID.Stone, TileID.GrayBrick);
+		DustType = ModContent.DustType<RadonMossDust>();
+	}
+
+	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+	{
+		Lighting.AddLight(new Vector2(i, j).ToWorldCoordinates(), Color.Yellow.ToVector3() * .3f);
+		return true;
+	}
+
+	public override void RandomUpdate(int i, int j)
+	{
+		if (SpreadHelper.Spread(i, j, Type, 1, DirtType) && Main.netMode != NetmodeID.SinglePlayer)
 			NetMessage.SendTileSquare(-1, i, j, 3, TileChangeType.None); //Try spread moss
 
 		GrowTiles(i, j);
@@ -68,7 +112,7 @@ public class RadonPlants : ModTile
 
 		TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
 		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
-		TileObjectData.newTile.AnchorAlternateTiles = [ModContent.TileType<RadonMoss>()];
+		TileObjectData.newTile.AnchorAlternateTiles = [ModContent.TileType<RadonMoss>(), ModContent.TileType<RadonMossGrayBrick>()];
 		TileObjectData.newTile.StyleHorizontal = true;
 		TileObjectData.newTile.RandomStyleRange = StyleRange;
 
@@ -89,12 +133,27 @@ public class RadonPlants : ModTile
 
 		TileObjectData.addTile(Type);
 
-		AddMapEntry(new Color(252, 248, 3));
+		AddMapEntry(new Color(206, 209, 23));
 		DustType = ModContent.DustType<RadonMossDust>();
 	}
 
-	public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) => offsetY = 2;
+	public override IEnumerable<Item> GetItemDrops(int i, int j)
+	{
+		int heldType = Main.player[Player.FindClosest(new Vector2(i, j).ToWorldCoordinates(0, 0), 16, 16)].HeldItem.type;
 
+		if (heldType == ItemID.PaintScraper || heldType == ItemID.SpectrePaintScraper)
+		{
+			if (Main.rand.NextBool(9))
+				yield return new Item(ModContent.ItemType<RadonMossItem>());
+		}
+	}
+
+	public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY)
+	{
+		int style = tileFrameX / 18;
+		if (style < StyleRange * 3)
+			offsetY = 2;
+	}
 	public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
 	{
 		Lighting.AddLight(new Vector2(i, j).ToWorldCoordinates(), Color.Yellow.ToVector3() * .35f);
