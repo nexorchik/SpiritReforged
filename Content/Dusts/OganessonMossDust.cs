b116036e3a5bff4dd005de75bdcc9e4aea525c56
Abhay M <amanchala13@gmail.com>
2025-04-08 14:57:38 -0700

    Oganesson

---
+++ Content/Dusts/OganessonMossDust.cs
@@ -0,0 +1,28 @@
+﻿namespace SpiritReforged.Content.Dusts;
+
+public class OganessonMossDust : ModDust
+{
+	public override void OnSpawn(Dust dust)
+	{
+		dust.noGravity = true;
+		dust.noLight = false;
+		dust.color.R = 220;
+		dust.color.G = 220;
+		dust.color.B = 220;
+	}
+
+	public override Color? GetAlpha(Dust dust, Color lightColor) => new Color(220, 220, 220, 60);
+	public override bool Update(Dust dust)
+	{
+		dust.position += dust.velocity;
+		dust.velocity *= 0.88f;
+		dust.velocity.Y -= .2f;
+
+		dust.rotation = dust.velocity.ToRotation();
+
+		if ((dust.scale *= 0.95f) < 0.2f)
+			dust.active = false;
+
+		return false;
+	}
+}
---
+++ Content/Underground/Moss/Oganesson/OganessonMossItem.cs
@@ -0,0 +1,69 @@
+using SpiritReforged.Common.Visuals.Glowmasks;
+
+namespace SpiritReforged.Content.Underground.Moss.Oganesson;
+
+[AutoloadGlowmask("255,255,255")]
+public class OganessonMossItem : ModItem
+{
+	public override void SetStaticDefaults()
+	{
+		ItemID.Sets.DisableAutomaticPlaceableDrop[Type] = true;
+		Item.ResearchUnlockCount = 25;
+		ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.RainbowMoss;
+	}
+
+	public override void SetDefaults()
+	{
+		Item.width = Item.height = 16;
+		Item.useAnimation = 15;
+		Item.useTime = 10;
+		Item.maxStack = Item.CommonMaxStack;
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.useTurn = true;
+		Item.autoReuse = true;
+		Item.consumable = true;
+	}
+
+	public override void HoldItem(Player player)
+	{
+		if (player.IsTargetTileInItemRange(Item))
+		{
+			player.cursorItemIconEnabled = true;
+			player.cursorItemIconID = Type;
+		}
+	}
+
+	public override void Update(ref float gravity, ref float maxFallSpeed) => Lighting.AddLight(Item.position, .252f, .252f, .252f);
+
+	public override bool? UseItem(Player player)
+	{
+		if (Main.myPlayer == player.whoAmI)
+		{
+			Tile tile = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY);
+			if (tile.HasTile && player.IsTargetTileInItemRange(Item))
+			{
+				if (tile.TileType == TileID.Stone)
+				{
+					WorldGen.PlaceTile(Player.tileTargetX, Player.tileTargetY, ModContent.TileType<OganessonMoss>(), forced: true);
+
+					if (Main.netMode != NetmodeID.SinglePlayer)
+						NetMessage.SendTileSquare(player.whoAmI, Player.tileTargetX, Player.tileTargetY);
+
+					return true;
+				}
+
+				if (tile.TileType == TileID.GrayBrick)
+				{
+					WorldGen.PlaceTile(Player.tileTargetX, Player.tileTargetY, ModContent.TileType<OganessonMossGrayBrick>(), forced: true);
+
+					if (Main.netMode != NetmodeID.SinglePlayer)
+						NetMessage.SendTileSquare(player.whoAmI, Player.tileTargetX, Player.tileTargetY);
+
+					return true;
+				}
+			}
+		}
+
+		return null;
+	}
+}
