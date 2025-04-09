6b9d3e4f30226254e00b0e7252fbc118a853ae6b
Abhay M <amanchala13@gmail.com>
2025-04-09 13:09:48 -0700

    Start Thorium Crossmod
    - Add ThoriumCompat
    - Add Drops to Packing Crate

---
+++ Common/ModCompat/ThoriumCompat.cs
@@ -0,0 +1,14 @@
+﻿namespace SpiritReforged.Common.ModCompat;
+
+internal class ThoriumCompat : ModSystem
+{
+	public static Mod Instance;
+	public static bool Enabled => Instance != null;
+
+	public override void Load()
+	{
+		Instance = null;
+		if (!ModLoader.TryGetMod("ThoriumMod", out Instance))
+			return;
+	}
+}
