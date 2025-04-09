38c20734bc79922e01332a24c2bfe8e7809e7a90
Abhay M <amanchala13@gmail.com>
2025-04-09 14:50:57 -0700

    NPC shops

--- Common/ModCompat/ThoriumCompat.cs
+++ Common/ModCompat/ThoriumCompat.cs
@@ -1,6 +1,11 @@
 ﻿
+using SpiritReforged.Common.Misc;
+using SpiritReforged.Content.Forest.Cloud.Items;
 using SpiritReforged.Content.Ocean.Items.JellyfishStaff;
+using SpiritReforged.Content.Savanna.Tiles.Paintings;
+using SpiritReforged.Content.Underground.ExplorerTreads;
 using SpiritReforged.Content.Vanilla.Food;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace SpiritReforged.Common.ModCompat;
@@ -34,6 +39,24 @@ internal class ThoriumGlobalNPC : GlobalNPC
 			{
 				if (npc.type == bigJellyfish.Type)
 					npcLoot.AddCommon(ModContent.ItemType<JellyfishStaff>(), 50);
+			}
+		}
+	}
+
+	public override void ModifyShop(NPCShop shop)
+	{
+		if (ThoriumCompat.Enabled)
+		{
+			if (ThoriumCompat.Instance.TryFind("Cobbler", out ModNPC cobbler))
+			{
+				if (shop.NpcType == cobbler.Type)
+					shop.Add(ModContent.ItemType<ExplorerTreadsItem>(), Condition.DownedEyeOfCthulhu);
+			}
+
+			if (ThoriumCompat.Instance.TryFind("Druid", out ModNPC druid))
+			{
+				if (shop.NpcType == druid.Type)
+					shop.Add(ModContent.ItemType<CloudstalkSeed>(), Condition.DownedEyeOfCthulhu);
 			}
 		}
 	}
