c267ddf616e5b561dc8e4d4f94661882343f6d92
Abhay M <amanchala13@gmail.com>
2025-04-09 14:03:05 -0700

    ThoriumGlobalNPC

--- Common/ModCompat/ThoriumCompat.cs
+++ Common/ModCompat/ThoriumCompat.cs
@@ -1,4 +1,9 @@
-﻿namespace SpiritReforged.Common.ModCompat;
+﻿
+using SpiritReforged.Content.Ocean.Items.JellyfishStaff;
+using SpiritReforged.Content.Vanilla.Food;
+using Terraria.ModLoader;
+
+namespace SpiritReforged.Common.ModCompat;
 
 internal class ThoriumCompat : ModSystem
 {
@@ -10,5 +15,26 @@ internal class ThoriumCompat : ModSystem
 		Instance = null;
 		if (!ModLoader.TryGetMod("ThoriumMod", out Instance))
 			return;
+	}
+}
+
+internal class ThoriumGlobalNPC : GlobalNPC
+{
+	public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+	{
+		if (ThoriumCompat.Enabled)
+		{
+			if (ThoriumCompat.Instance.TryFind("RagingMinotaur", out ModNPC minotaur))
+			{
+				if (npc.type == minotaur.Type)
+					npcLoot.AddCommon(ItemID.Leather, 1, 5, 10);
+			}
+
+			if (ThoriumCompat.Instance.TryFind("ManofWar", out ModNPC bigJellyfish))
+			{
+				if (npc.type == bigJellyfish.Type)
+					npcLoot.AddCommon(ModContent.ItemType<JellyfishStaff>(), 50);
+			}
+		}
 	}
 }
