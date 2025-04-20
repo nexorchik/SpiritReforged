2caaa4fb0cfc93e963c12c6451e4ed77e4d4d54e
GabeHasWon <gabriel@guevarameyer.com>
2025-04-19 23:42:20 -0500

    start cave entrance framework

---
+++ Common/WorldGeneration/Micropasses/Passes/CustomCaveEntranceMicropass.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.WorldBuilding;
+
+namespace SpiritReforged.Common.WorldGeneration.Micropasses.Passes;
+
+internal class CustomCaveEntranceMicropass : Micropass
+{
+	public override string WorldGenName => "Cave Entrances";
+
+	public override void Load(Mod mod) => On_WorldGen.Mountinater += OverrideGenMound;
+
+	private void OverrideGenMound(On_WorldGen.orig_Mountinater orig, int i, int j)
+	{
+		//orig(i, j);
+	}
+
+	public override int GetWorldGenIndexInsert(List<GenPass> passes, ref bool afterIndex) => passes.FindIndex(genpass => genpass.Name.Equals("Sunflowers"));
+
+	public override void Run(GenerationProgress progress, Terraria.IO.GameConfiguration config)
+	{
+	}
+}
