4bb10be123aff139224f924175ee2264e7225e35
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-04-30 01:27:24 -0300

    Frost Giant Belt

---
+++ Content/Snow/FrostGiantBelt.cs
@@ -0,0 +1,45 @@
+using SpiritReforged.Common.ItemCommon;
+using SpiritReforged.Common.NPCCommon;
+using SpiritReforged.Common.PlayerCommon;
+using Terraria.GameContent.ItemDropRules;
+
+namespace SpiritReforged.Content.Snow;
+
+[AutoloadEquip(EquipType.Waist)]
+public class FrostGiantBelt : AccessoryItem
+{
+	public static bool HoldingClub(Player player) => player.HeldItem?.ModItem is ClubItem;
+	public override void SetStaticDefaults() => NPCLootDatabase.AddLoot(new(NPCLootDatabase.MatchId(NPCID.UndeadViking), ItemDropRule.Common(Type, 15)));
+
+	public override void SetDefaults()
+	{
+		Item.Size = new Vector2(30);
+		Item.value = Item.sellPrice(0, 0, 90, 0);
+		Item.rare = ItemRarityID.Orange;
+		Item.accessory = true;
+	}
+}
+
+internal class FrostGiantPlayer : ModPlayer
+{
+	public float extraDefense;
+
+	public override void UpdateEquips()
+	{
+		if (Player.HasAccessory<FrostGiantBelt>() && FrostGiantBelt.HoldingClub(Player) && Player.channel)
+		{
+			extraDefense = Math.Min(extraDefense + (float)(1f / 6f), 15);
+			Player.statDefense += (int)extraDefense;
+		}
+		else
+		{
+			extraDefense = 0;
+		}
+	}
+
+	public override void ModifyHurt(ref Player.HurtModifiers modifiers)
+	{
+		if (Player.HasAccessory<FrostGiantBelt>() && FrostGiantBelt.HoldingClub(Player) && Player.channel)
+			modifiers.Knockback *= 0.5f;
+	}
+}
