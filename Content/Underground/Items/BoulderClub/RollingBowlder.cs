7c62b0fdacfe37bdcdfcf0dc6423bee076728e65
QuestionMark <61338436+QuestionMark810@users.noreply.github.com>
2025-05-20 18:21:49 -0300

    Safekeeper death fix

--- Content/Underground/Items/BoulderClub/RollingBowlder.cs
+++ Content/Underground/Items/BoulderClub/RollingBowlder.cs
@@ -103,8 +103,7 @@ class RollingBowlder : ModProjectile
 	public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
 	{
 		fallThrough = false;
-
-		return base.TileCollideStyle(ref width, ref height, ref fallThrough, ref hitboxCenterFrac);
+		return true;
 	}
 
 	public override bool PreDraw(ref Color lightColor)
