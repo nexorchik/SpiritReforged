eb86f5223552effd368d8699de92b2dc00a824e3
GabeHasWon <gabriel@guevarameyer.com>
2025-06-03 15:06:26 -0500

    add placeholder recipe for savanna campfire

--- Content/Savanna/Tiles/SavannaCampfire.cs
+++ Content/Savanna/Tiles/SavannaCampfire.cs
@@ -12,6 +12,8 @@ public class SavannaCampfire : ModTile, IAutoloadTileItem
 	private static Asset<Texture2D> glowTexture;
 	private const int fullFrameHeight = 18 * 2;
 
+	public void AddItemRecipes(ModItem item) => item.CreateRecipe().AddRecipeGroup(RecipeGroupID.Wood, 10).AddIngredient(ItemID.Torch, 5).Register();
+
 	private static bool OnFire(int i, int j) => Main.tile[i, j].TileFrameY < fullFrameHeight;
 
 	public override void Load() => glowTexture = ModContent.Request<Texture2D>(Texture + "_Glow");
