bb78ffa019df7f68a8c0fed5f1932e18183a6dfb
PhoenixBladez <amanchala13@gmail.com>
2025-06-07 18:38:45 -0700

    census compat

---
+++ Common/ModCompat/CensusCompat.cs
@@ -0,0 +1,14 @@
+﻿using SpiritReforged.Content.Underground.NPCs;
+using static AssGen.Assets;
+
+namespace SpiritReforged.Common.ModCompat;
+
+internal class CensusCompat : ModSystem
+{
+	public override bool IsLoadingEnabled(Mod mod) => CrossMod.Census.Enabled;
+	public override void PostSetupContent()
+	{
+		var census = CrossMod.Census.Instance;
+		census.Call("TownNPCCondition", ModContent.NPCType<PotterySlime>(), ModContent.GetInstance<PotterySlime>().GetLocalization("Census.SpawnCondition"));
+	}
+}
